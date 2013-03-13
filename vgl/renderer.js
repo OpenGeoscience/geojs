9ae30e5526ea95942dbd7cd2eda5c4cc345f79c8
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-03-13 00:50:05 -0400

    Changed default background color to white

--- vgl/renderer.js
+++ vgl/renderer.js
@@ -65,7 +65,7 @@ vglModule.renderer = function() {
    * Render the scene
    */
   this.render = function() {
-    gl.clearColor(0.5, 0.5, 0.5, 1.0);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
