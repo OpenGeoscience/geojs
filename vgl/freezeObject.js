1691e2db27233ce1674ec7d7e2e6488374b72848
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-04-30 21:00:50 -0400

    Added module to freeze JS object

---
+++ vgl/freezeObject.js
@@ -0,0 +1,23 @@
+ /**
+ * @module ogs.vgl
+ */
+
+vglModule.freezeObject = function(obj) {
+  "use strict";
+
+  /**
+   * Freezes an object, using Object.freeze if available, otherwise returns
+   * the object unchanged.  This function should be used in setup code to prevent
+   * errors from completely halting JavaScript execution in legacy browsers.
+   *
+   * @exports freezeObject
+   */
+  var freezedObject = Object.freeze;
+  if (typeof freezedObject === 'undefined') {
+    freezedObject = function(o) {
+      return o;
+    };
+  }
+
+  return freezedObject;
+};
