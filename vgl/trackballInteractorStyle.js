1fd02f5bd96f99ad628ce7989aa64268413c3be5
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-05-05 23:23:05 -0400

    Fixed calculation of zoom delta

--- vgl/trackballInteractorStyle.js
+++ vgl/trackballInteractorStyle.js
@@ -38,7 +38,6 @@ vglModule.trackballInteractorStyle = function() {
       m_dx,
       m_dy,
       m_dz,
-      m_zTrans,
       m_mouseLastPos = {
         x: 0,
         y: 0
@@ -75,22 +74,24 @@ vglModule.trackballInteractorStyle = function() {
     if (m_outsideCanvas === true) {
       return;
     }
+    m_focalPoint = m_camera.focalPoint();
+    m_focusWorldPt = vec4.fromValues(m_focalPoint[0], m_focalPoint[1], m_focalPoint[2], 1);
+    m_focusDisplayPt = m_renderer.worldToDisplay(m_focusWorldPt, m_camera.viewMatrix(),
+      m_camera.projectionMatrix(), m_width, m_height);
+    m_displayPt1 = vec4.fromValues(
+      m_currentMousePos.x, m_currentMousePos.y, m_focusDisplayPt[2], 1.0);
+    m_displayPt2 = vec4.fromValues(
+      m_mouseLastPos.x, m_mouseLastPos.y, m_focusDisplayPt[2], 1.0);
+    m_worldPt1 = m_renderer.displayToWorld(
+      m_displayPt1, m_camera.viewMatrix(), m_camera.projectionMatrix(), m_width, m_height);
+    m_worldPt2 = m_renderer.displayToWorld(
+      m_displayPt2, m_camera.viewMatrix(), m_camera.projectionMatrix(), m_width, m_height);
+
+    m_dx = m_worldPt1[0] - m_worldPt2[0];
+    m_dy = m_worldPt1[1] - m_worldPt2[1];
+    m_dz = m_worldPt1[2] - m_worldPt2[2];
+
     if (m_middileMouseButtonDown) {
-      m_focalPoint = m_camera.focalPoint();
-      m_focusWorldPt = vec4.fromValues(m_focalPoint[0], m_focalPoint[1], m_focalPoint[2], 1);
-      m_focusDisplayPt = m_renderer.worldToDisplay(m_focusWorldPt, m_camera.viewMatrix(),
-        m_camera.projectionMatrix(), m_width, m_height);
-      m_displayPt1 = vec4.fromValues(
-        m_currentMousePos.x, m_currentMousePos.y, m_focusDisplayPt[2], 1.0);
-      m_displayPt2 = vec4.fromValues(
-        m_mouseLastPos.x, m_mouseLastPos.y, m_focusDisplayPt[2], 1.0);
-      m_worldPt1 = m_renderer.displayToWorld(
-        m_displayPt1, m_camera.viewMatrix(), m_camera.projectionMatrix(), m_width, m_height);
-      m_worldPt2 = m_renderer.displayToWorld(
-        m_displayPt2, m_camera.viewMatrix(), m_camera.projectionMatrix(), m_width, m_height);
-      m_dx = m_worldPt1[0] - m_worldPt2[0];
-      m_dy = m_worldPt1[1] - m_worldPt2[1];
-      m_dz = m_worldPt1[2] - m_worldPt2[2];
       m_camera.pan(-m_dx, -m_dy, -m_dz);
       $(m_that).trigger(vglModule.command.middleButtonPressEvent);
     }
@@ -100,8 +101,7 @@ vglModule.trackballInteractorStyle = function() {
       $(m_that).trigger(vglModule.command.leftButtonPressEvent);
     }
     if (m_rightMouseButtonDown) {
-      m_zTrans = m_currentMousePos.y - m_mouseLastPos.y;
-      m_camera.zoom(m_zTrans * 0.5);
+      m_camera.zoom(m_dy);
       $(m_that).trigger(vglModule.command.rightButtonPressEvent);
     }
     m_mouseLastPos.x = m_currentMousePos.x;
