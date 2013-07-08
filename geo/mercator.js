4122beba5e85e89fc1e9b7dcfb045dd093859d41
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-07-08 16:30:18 -0400

    Fixed style

--- geo/mercator.js
+++ geo/mercator.js
@@ -10,14 +10,9 @@
 /*vglModule, document*/
 //////////////////////////////////////////////////////////////////////////////
 
-/**
- * Create a new instance of class map
- *
- * @class Creates a new map inside of the given HTML container (Typically DIV)
- * @returns {geoModule.map}
- */
 geoModule.mercator = {};
 
+//////////////////////////////////////////////////////////////////////////////
 /**
  * Convert Longitute (Degree) to Tile X
  *
@@ -25,13 +20,15 @@ geoModule.mercator = {};
  *  @param {float, integer}
  *  @returns {integer}
  */
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.long2tilex = function(lon, z) {
+  "use strict";
   var rad = (lon + 180.0) / 360.0;
   var f = Math.floor(rad * Math.pow(2.0, z));
   return Math.round(f)
-}
+};
 
-
+//////////////////////////////////////////////////////////////////////////////
 /**
  * Convert Latitude (Degree) to Tile Y
  *
@@ -39,11 +36,14 @@ geoModule.mercator.long2tilex = function(lon, z) {
  *  @param {float, integer}
  *  @returns {integer}
  */
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.lat2tiley = function(lat, z) {
+  "use strict";
   var rad = lat * Math.PI/180.0;
   return Math.round(Math.floor((1.0 - rad / Math.PI) / 2.0 * Math.pow(2.0, z)));
-}
+};
 
+//////////////////////////////////////////////////////////////////////////////
 /**
  * Convert Longitute (Degree) to Tile X and fraction.
  *
@@ -51,15 +51,17 @@ geoModule.mercator.lat2tiley = function(lat, z) {
  *  @param {float, integer}
  *  @returns {integer, float}
  */
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.long2tilex2 = function(lon, z) {
+  "use strict";
   var rad = (lon + 180.0) / 360.0;
   var f = rad * Math.pow(2.0, z);
   var ret = Math.floor(f);
   var frac = f-ret;
   return [ret, frac];
-}
+};
 
-
+//////////////////////////////////////////////////////////////////////////////
 /**
  * Convert Latitude (Degree) to Tile Y and fraction
  *
@@ -67,13 +69,17 @@ geoModule.mercator.long2tilex2 = function(lon, z) {
  *  @param {float, integer}
  *  @returns {integer, float}
  */
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.lat2tiley2 = function(lat, z) {
+  "use strict";
   var rad = lat * Math.PI/180.0;
   var f = (1.0 - Math.log( Math.tan(rad) + 1.0 / Math.cos(rad)) / Math.PI) / 2.0 * Math.pow(2.0, z);
   var ret = Math.floor(f);
   var frac = f-ret;
   return [ret, frac];
-}
+};
+
+//////////////////////////////////////////////////////////////////////////////
 /**
  * Convert Tile X to Longitute (Degree)
  *
@@ -81,12 +87,13 @@ geoModule.mercator.lat2tiley2 = function(lat, z) {
  *  @param {integer, integer}
  *  @returns {float}
  */
-
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.tilex2long = function(x, z) {
+  "use strict";
   return x / pow(2.0, z) * 360.0 - 180.0;
-}
+};
 
-
+//////////////////////////////////////////////////////////////////////////////
 /**
  * Convert Tile Y to Latitute (Degree)
  *
@@ -94,16 +101,37 @@ geoModule.mercator.tilex2long = function(x, z) {
  *  @param {integer, integer}
  *  @returns {float}
  */
-
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.tiley2lat = function(y, z) {
+  "use strict";
   n = Math.PI - 2.0 * Math.PI * y / pow(2.0, z);
   return 180.0 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
-}
+};
 
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * Convert spherical mercator Y to latitude
+ *
+ *  @method y2lat
+ *  @param {float}
+ *  @returns {float}
+ */
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.y2lat = function(a) {
+  "use strict";
  return 180/Math.PI * (2 * Math.atan(Math.exp(a*Math.PI/180)) - Math.PI/2);
-}
+};
 
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * Convert latitude into Y position in spherical mercator
+ *
+ *  @method lat2y
+ *  @param {float}
+ *  @returns {float}
+ */
+//////////////////////////////////////////////////////////////////////////////
 geoModule.mercator.lat2y = function(a) {
+  "use strict";
   return 180/Math.PI * Math.log(Math.tan(Math.PI/4+a*(Math.PI/180)/2));
-}
+};
