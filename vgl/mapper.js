12ac554a0d49f949f080d50362a6849552232bb7
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-09-03 18:06:39 -0400

    Fixed mapper

--- vgl/mapper.js
+++ vgl/mapper.js
@@ -35,6 +35,94 @@ vglModule.mapper = function() {
 
   ////////////////////////////////////////////////////////////////////////////
   /**
+   * Delete cached VBO if any
+   *
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  function deleteVertexBufferObjects() {
+    var i;
+    for (i = 0; i < m_buffers.length; ++i) {
+      gl.deleteBuffer(m_buffers[i]);
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Create new VBO for all its geometryData sources and primitives
+   *
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  function createVertexBufferObjects() {
+    if (m_geomData) {
+      var numberOfSources = m_geomData.numberOfSources(),
+          i, j, k, bufferId = null, keys, ks, numberOfPrimitives;
+
+      for (i = 0; i < numberOfSources; ++i) {
+        bufferId = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+        gl.bufferData(gl.ARRAY_BUFFER,
+          new Float32Array(m_geomData.source(i).data()), gl.STATIC_DRAW);
+
+        keys = m_geomData.source(i).keys();
+        ks = [];
+
+        for (j = 0; j < keys.length; ++j) {
+          ks.push(keys[j]);
+        }
+
+        m_bufferVertexAttributeMap[i] = ks;
+        m_buffers[i] = bufferId;
+      }
+
+      numberOfPrimitives = m_geomData.numberOfPrimitives();
+      for (k = 0; k < numberOfPrimitives; ++k) {
+        bufferId = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, bufferId);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, m_geomData.primitive(k)
+            .indices(), gl.STATIC_DRAW);
+        m_buffers[i++] = bufferId;
+      }
+
+      m_glCompileTimestamp.modified();
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Clear cache related to buffers
+   *
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  function cleanUpDrawObjects() {
+    m_bufferVertexAttributeMap = {};
+    m_buffers = [];
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Setup draw objects; Delete old ones and create new ones
+   *
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  function setupDrawObjects() {
+    // Delete buffer objects from past if any.
+    deleteVertexBufferObjects();
+
+    // Clear any cache related to buffers
+    cleanUpDrawObjects();
+
+    // Now construct the new ones.
+    createVertexBufferObjects();
+
+    m_dirty = false;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
    * Compute bounds of the data
    */
   ////////////////////////////////////////////////////////////////////////////
@@ -143,94 +231,6 @@ vglModule.mapper = function() {
                       primitive.indicesValueType(), 0);
     }
   };
-
-  ////////////////////////////////////////////////////////////////////////////
-  /**
-   * Delete cached VBO if any
-   *
-   * @private
-   */
-  ////////////////////////////////////////////////////////////////////////////
-  function deleteVertexBufferObjects() {
-    var i;
-    for (i = 0; i < m_buffers.length; ++i) {
-      gl.deleteBuffer(m_buffers[i]);
-    }
-  }
-
-  ////////////////////////////////////////////////////////////////////////////
-  /**
-   * Create new VBO for all its geometryData sources and primitives
-   *
-   * @private
-   */
-  ////////////////////////////////////////////////////////////////////////////
-  function createVertexBufferObjects() {
-    if (m_geomData) {
-      var numberOfSources = m_geomData.numberOfSources(),
-          i, j, k, bufferId = null, keys, ks, numberOfPrimitives;
-
-      for (i = 0; i < numberOfSources; ++i) {
-        bufferId = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-        gl.bufferData(gl.ARRAY_BUFFER,
-          new Float32Array(m_geomData.source(i).data()), gl.STATIC_DRAW);
-
-        keys = m_geomData.source(i).keys();
-        ks = [];
-
-        for (j = 0; j < keys.length; ++j) {
-          ks.push(keys[j]);
-        }
-
-        m_bufferVertexAttributeMap[i] = ks;
-        m_buffers[i] = bufferId;
-      }
-
-      numberOfPrimitives = m_geomData.numberOfPrimitives();
-      for (k = 0; k < numberOfPrimitives; ++k) {
-        bufferId = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, bufferId);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, m_geomData.primitive(k)
-            .indices(), gl.STATIC_DRAW);
-        m_buffers[i++] = bufferId;
-      }
-
-      m_glCompileTimestamp.modified();
-    }
-  }
-
-  ////////////////////////////////////////////////////////////////////////////
-  /**
-   * Clear cache related to buffers
-   *
-   * @private
-   */
-  ////////////////////////////////////////////////////////////////////////////
-  function cleanUpDrawObjects() {
-    m_bufferVertexAttributeMap = {};
-    m_buffers = [];
-  }
-
-  ////////////////////////////////////////////////////////////////////////////
-  /**
-   * Setup draw objects; Delete old ones and create new ones
-   *
-   * @private
-   */
-  ////////////////////////////////////////////////////////////////////////////
-  function setupDrawObjects() {
-    // Delete buffer objects from past if any.
-    deleteVertexBufferObjects();
-
-    // Clear any cache related to buffers
-    cleanUpDrawObjects();
-
-    // Now construct the new ones.
-    createVertexBufferObjects();
-
-    m_dirty = false;
-  }
 
   return this;
 };
