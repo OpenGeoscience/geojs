2dde432dd961cfd999a80b7b4a458ebf9d01b25f
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-09-03 18:10:15 -0400

    Fixed shader

--- vgl/shader.js
+++ vgl/shader.js
@@ -6,7 +6,7 @@
 /*jslint devel: true, forin: true, newcap: true, plusplus: true*/
 /*jslint white: true, continue:true, indent: 2*/
 
-/*global vglModule, ogs, vec4, inherit, $*/
+/*global gl, vglModule, ogs, vec4, inherit, $*/
 //////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////
