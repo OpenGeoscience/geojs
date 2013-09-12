35265c14ac1ef7f47509ece553a251fc874ad94d
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-09-12 17:30:35 -0400

    Fixed time

--- geo/time.js
+++ geo/time.js
@@ -1,16 +1,29 @@
-geoModule.time = {}
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * @module ogs.geo
+ */
+
+/*jslint devel: true, forin: true, newcap: true, plusplus: true*/
+/*jslint white: true, indent: 2*/
+
+/*global geoModule, ogs, inherit, $, HTMLCanvasElement, Image*/
+/*global vglModule, document*/
+//////////////////////////////////////////////////////////////////////////////
+
+geoModule.time = {};
 
 geoModule.time.incrementTime = function (time, unit, delta) {
+  'use strict';
 
-  if (unit == 'days') {
+  if (unit === 'days') {
     time.setDate(time.getDate() + delta);
   }
-  else if (unit == 'months') {
+  else if (unit === 'months') {
     time.setMonth(time.getMonth() + delta);
   }
-  else if (unit == 'years') {
+  else if (unit === 'years') {
     time.setYear(time.getYear() + delta);
   }
 
   return time;
-}
+};
