7f435180da8ab2062089fccc88dc588f54c3c342
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-03-05 09:44:27 -0500

    removing vgl from jslint global where not necessary

--- src/core/data.js
+++ src/core/data.js
@@ -7,7 +7,7 @@
 /*jslint white: true, indent: 2*/
 
 /*global geo, ogs, inherit, $, HTMLCanvasElement, Image*/
-/*vgl, document*/
+/*document*/
 //////////////////////////////////////////////////////////////////////////////
 geo.countries = {
   "type": "FeatureCollection",
