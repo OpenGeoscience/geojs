fbcaa0ad890a7673454ef184822d5d7e4e53bd41
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-03-24 11:54:15 -0400

    First pass on layer gcs transformation

--- src/core/geoTransform.js
+++ src/core/geoTransform.js
@@ -119,7 +119,7 @@ geo.geoTransform.osmTransformFeature = function(destGcs, feature) {
  * Transform a feature to destination GCS
  */
 //////////////////////////////////////////////////////////////////////////////
-geo.geoTransform.transformFeature = function(destGcs, feature) {
+geo.geoTransform.transformFeature = function(destGcs, feature, inplace) {
   'use strict';
 
   if (!feature) {
@@ -131,95 +131,64 @@ geo.geoTransform.transformFeature = function(destGcs, feature) {
     return;
   }
 
-  var geometryDataArray = [],
-      noOfGeoms = 0,
-      index = 0,
-      geometryData = null,
-      posSourceData = null,
-      data = null,
-      noOfComponents = null,
-      stride = null,
-      offset = null,
-      sizeOfDataType = null,
-      count = null,
-      i = 0,
-      ib = 0,
-      jb = 0,
-      value = null,
-      inPos = [],
-      projPoint = null,
-      vertexPos = null,
-      srcGcs = feature.gcs(),
-      source = new proj4.Proj(srcGcs),
-      dest = new proj4.Proj(destGcs);
-
-  if (feature.mapper() instanceof vgl.groupMapper) {
-    geometryDataArray = feature.mapper().geometryDataArray();
-  } else {
-    geometryDataArray.push(feature.mapper().geometryData());
+  if (!(feature instanceof geo.pointFeature)) {
+    throw "Supports only point feature";
   }
 
-  noOfGeoms = geometryDataArray.length;
+  var noOfComponents = null, count = null, inPos = null, outPos = null,
+      projPoint = null, srcGcs = feature.gcs(), i, inplace = inplace || false,
+      source = new proj4.Proj(srcGcs), dest = new proj4.Proj(destGcs);
 
-  for (index = 0; index < noOfGeoms; ++index) {
-    geometryData = geometryDataArray[index];
-    posSourceData = geometryData.sourceData(
-      vgl.vertexAttributeKeys.Position);
-    data = posSourceData.data();
-    noOfComponents = posSourceData.attributeNumberOfComponents(
-      vgl.vertexAttributeKeys.Position);
-    stride = posSourceData.attributeStride(
-      vgl.vertexAttributeKeys.Position);
-    offset = posSourceData.attributeOffset(
-      vgl.vertexAttributeKeys.Position);
-    sizeOfDataType = posSourceData.sizeOfAttributeDataType(
-      vgl.vertexAttributeKeys.Position);
-    count = data.length;
+  source = new proj4.Proj(srcGcs);
+  dest = new proj4.Proj(destGcs);
 
-    source = new proj4.Proj(srcGcs);
-    dest = new proj4.Proj(destGcs);
+  if (feature instanceof geo.pointFeature) {
+    inPos = feature.inPositions();
+    count = inPos.length
 
-    if (noOfComponents < 2 || noOfComponents > 3) {
-      console.log('[error] Geotransform requires 2d or 3d points.');
-      console.log('[error] Geotransform got ', noOfComponents);
-      return;
+    if (!(inPos instanceof Array)) {
+      throw "Supports Array of 2D and 3D points";
     }
 
-    inPos.length = 3;
+    noOfComponents = (count % 2 === 0 ? 2 :
+                       (count % 3 === 0 ? 3 : null));
 
-    // We need to operate on arrays
-    stride /= sizeOfDataType;
-    offset /= sizeOfDataType;
+    if (noOfComponents !== 2 || noOfComponents !== 3) {
+      throw "Transform points require points in 2D or 3D";
+    }
 
-    for (i = 0; i < count; ++i) {
-      vertexPos = i * stride + offset;
-
+    for (i = 0; i < count; i += noOfComponents) {
       if (noOfComponents === 2) {
-        inPos[0] = data[vertexPos];
-        inPos[1] = data[vertexPos + 1];
-        inPos[2] = 0.0;
+        projPoint = new proj4.Point(inPos[i], inPos[i + 1], 0.0);
       } else {
-        inPos[0] = data[vertexPos];
-        inPos[1] = data[vertexPos + 1];
-        inPos[2] = data[vertexPos + 2];
+        projPoint = new proj4.Point(inPos[i], inPos[i + 1], inPos[i + 2]);
       }
 
-      projPoint = new proj4.Point(inPos[0], inPos[1], inPos[2]);
       proj4.transform(source, dest, projPoint);
 
-      if (noOfComponents === 2) {
-        data[vertexPos] =  projPoint.x;
-        data[vertexPos + 1] = projPoint.y;
+      if (inplace) {
+        outPos = inPos;
       } else {
-        data[vertexPos] = projPoint.x;
-        data[vertexPos + 1] = projPoint.y;
-        data[vertexPos + 2] = projPoint.z;
+        outPos = [];
+        outPos.length = inPos.length;
+      }
+
+      if (noOfComponents === 2) {
+        outPos[i] =  projPoint.x;
+        outPos[i + 1] = projPoint.y;
+      } else {
+        outPos[i] = projPoint.x;
+        outPos[i + 1] = projPoint.y;
+        outPos[i + 2] = projPoint.z;
       }
     }
+
+    feature.positions(outPos);
+    feature.gcs(destGcs);
+    return outPos;
   }
 
-  // Update the features gcs field
-  feature.setGcs(destGcs);
+  return null;
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -228,20 +197,40 @@ geo.geoTransform.transformFeature = function(destGcs, feature) {
  * projection.
  */
 //////////////////////////////////////////////////////////////////////////////
-geo.geoTransform.transformLayer = function(destGcs, layer) {
+geo.geoTransform.transformLayer = function(destGcs, layer, baseLayer) {
   'use strict';
 
+  var features, count, i;
+
   if (!layer) {
-    console.log('[warning] Invalid (null) layer');
+    throw "Requires valid layer for tranformation";
+  }
+
+  if (!baseLayer) {
+    throw "Requires baseLayer used by the map";
+  }
+
+  if (layer === baseLayer) {
     return;
   }
 
-  var features = layer.features(),
-      count = features.length,
-      i = 0;
-  for (i = 0; i < count; ++i) {
-    // TODO Ignoring src and destination projections
-    geo.geoTransform.osmTransformFeature(
-      destGcs, features[i]);
+  if (layer instanceof geo.featureLayer) {
+    features = layer.features();
+    count = features.length;
+    i = 0;
+
+    for (i = 0; i < count; ++i) {
+      if (destGcs === "EPSG:3857" && baseLayer instanceof geo.osmLayer) {
+        geo.geoTransform.osmTransformFeature(
+          destGcs, features[i], true);
+      } else {
+        geo.geoTransform.transformFeature(
+          destGcs, features[i], true);
+      }
+    }
+
+    layer.gcs(destGcs);
+  } else {
+    throw "Only feature layer transformation is supported";
   }
 };
