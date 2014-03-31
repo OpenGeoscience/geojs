00853d645b764a465d770948ca8c3a8793398d23
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-03-31 16:10:50 -0400

    adding jasmine unit testing framework with tests for object.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-      files: ['src/**/*.js', 'package.json', 'Gruntfile.js'],
-      tasks: ['shell']
+      files: ['src/**/*.js', 'package.json', 'Gruntfile.js', 'testing/spec/*spec.js'],
+      tasks: ['shell', 'jasmine']
     },
     
     shell: {
@@ -18,9 +18,26 @@ module.exports = function(grunt) {
           stdout: true
         }
       }
+    },
+
+    jasmine: {
+      unittests: {
+        src: [
+          'build/deploy/testing/common/js/jquery-1.9.1.js',
+          'build/deploy/testing/common/js/gl-matrix.js',
+          'build/deploy/testing/common/js/d3.v3.min.js',
+          'build/deploy/testing/common/js/proj4.js',
+          'build/deploy/web/lib/vgl.min.js',
+          'build/deploy/web/lib/geojs.min.js'
+        ],
+        options: {
+          specs: 'testing/spec/*spec.js'
+        }
+      }
     }
   });
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.registerTask('default', ['watch']);
 };
