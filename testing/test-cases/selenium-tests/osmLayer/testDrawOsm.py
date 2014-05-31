5f077b56232db7c7c69da75bfd85e89a557b896f
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-05-31 14:59:31 -0400

    adding a test for panning

--- testing/test-cases/selenium-tests/osmLayer/testDrawOsm.py
+++ testing/test-cases/selenium-tests/osmLayer/testDrawOsm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import time
 
 from selenium_test import FirefoxTest, ChromeTest,\
     setUpModule, tearDownModule
@@ -9,13 +10,21 @@ from selenium_test import FirefoxTest, ChromeTest,\
 class osmBase(object):
     testCase = ('osmLayer',)
 
-    def test_osm_draw(self):
-
-        testName = 'osmDraw'
+    def loadPage(self):
         self.loadURL('osmLayer/index.html')
-        self.wait()
         self.resizeWindow(640, 480)
+        self.wait()
 
+    def test_osm_draw(self):
+        testName = 'osmDraw'
+        self.loadPage()
+        self.screenshotTest(testName)
+
+    def test_osm_pan(self):
+        testName = 'osmPan'
+        self.loadPage()
+        self.drag('#map', (200, 150))
+        time.sleep(1)  # wait for tiles to load
         self.screenshotTest(testName)
 
 
