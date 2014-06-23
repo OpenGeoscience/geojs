f4de0cc08749a594d0ef7e44ca4f01f3b8ae9a08
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-06-23 16:26:33 -0400

    changing to jshint for style tests/installing by npm

--- cmake/FindUglifyJS.cmake
+++
@@ -1,7 +0,0 @@
-include(FindPackageHandleStandardArgs)
-
-if(NOT UglifyJS_EXECUTABLE)
-    find_program(UglifyJS_EXECUTABLE uglifyjs)
-endif()
-
-find_package_handle_standard_args(UglifyJS DEFAULT_MSG UglifyJS_EXECUTABLE)
