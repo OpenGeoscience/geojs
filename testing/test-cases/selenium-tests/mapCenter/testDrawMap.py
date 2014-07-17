1e3247a52df8ddcf0ddf5829b5f9cfb3b1a05d0a
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-07-17 12:38:41 -0400

    Added a test to center option for a map

---
+++ testing/test-cases/selenium-tests/mapCenter/testDrawMap.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import time
+
+from selenium_test import FirefoxTest, ChromeTest,\
+    setUpModule, tearDownModule
+
+
+class mapCenter(object):
+    testCase = ('mapCenter',)
+
+    def loadPage(self):
+        self.resizeWindow(640, 480)
+        self.loadURL('mapCenter/index.html')
+        self.wait()
+
+    def test_map_center(self):
+        testName = 'osmDraw'
+        self.loadPage()
+        self.screenshotTest(testName, revision=3)
+
+class FirefoxMapCenter(mapCenter, FirefoxTest):
+    testCase = mapCenter.testCase + ('firefox',)
+
+
+class ChromeMapCenter(mapCenter, ChromeTest):
+    testCase = mapCenter.testCase + ('chrome',)
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
