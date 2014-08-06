62a24b4219ca7062338fb71ad4ec0bfed717a2af
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-08-06 15:44:15 -0400

    Adding a method to get svg elements from d3 features

--- src/d3/graphFeature.js
+++ src/d3/graphFeature.js
@@ -1,10 +1,29 @@
 gd3.graphFeature = function (arg) {
   'use strict';
 
+  var m_this = this;
+
   if (!(this instanceof gd3.graphFeature)) {
     return new gd3.graphFeature(arg);
   }
   geo.graphFeature.call(this, arg);
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+  *  Returns a d3 selection for the graph elements
+  */
+  ////////////////////////////////////////////////////////////////////////////
+  this.select = function () {
+    var renderer = m_this.renderer(),
+        selection = [],
+        node = m_this.nodeFeature(),
+        links = m_this.linkFeatures();
+    selection = selection.concat(renderer.select(node._d3id()));
+    links.forEach(function (link) {
+      selection = selection.concat(renderer.select(link._d3id()));
+    });
+    return selection;
+  };
 
   return this;
 };
