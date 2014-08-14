61c6757a4146efa033f213e5f140dc71d308e9b3
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-08-14 09:18:23 -0400

    Adding test for proj4 conversion

--- testing/test-cases/jasmine-tests/coordinateConversion.js
+++ testing/test-cases/jasmine-tests/coordinateConversion.js
@@ -8,7 +8,7 @@ describe('coordinate conversion', function () {
     var pt = [90.0, 0.0], d, d0;
     var d = {x: pt[0], y: pt[1]};
     d0 = geo.transform.transformCoordinates("EPSG:3857",
-               "EPSG:3857", d)
+               "EPSG:3857", d);
     expect(d0.x).toBeCloseTo(d.x, 0);
     expect(d0.y).toBeCloseTo(d.y, 0);
   });
@@ -17,7 +17,7 @@ describe('coordinate conversion', function () {
     var pt = [0.0, 90.0], d, d0;
     var d = {x: pt[0], y: pt[1]};
     d0 = geo.transform.transformCoordinates("EPSG:4326",
-               "EPSG:3857", d)
+               "EPSG:3857", d);
     expect(d0.x).toBeCloseTo(d.x, 0);
     expect(d0.y).toBeCloseTo(180.0, 1);
   });
@@ -26,16 +26,25 @@ describe('coordinate conversion', function () {
     var pt = [0.0, 90.0], d, d0;
     var d = geo.latlng(pt[1], pt[0]);
     d0 = geo.transform.transformCoordinates("EPSG:4326",
-               "EPSG:3857", d)
+               "EPSG:3857", d);
     expect(d0.x()).toBeCloseTo(d.x(), 0);
     expect(d0.y()).toBeCloseTo(180.0, 1);
+  });
+
+  it('platcaree to azimuthal equidistant projection', function () {
+    var pt = [18.5, 54.2], d, d0;
+    var d = geo.latlng(pt[1], pt[0]);
+    d0 = geo.transform.transformCoordinates("EPSG:4326",
+               "EPSG:9810", d);
+    expect(d0.x()).toBeCloseTo(1274403.37141,  0.1);
+    expect(d0.y()).toBeCloseTo(-3808790.19261, 0.1);
   });
 
   it('osm to platcaree projection array of array', function () {
     var pt = [[0.0, 90.0], [0.0, 0.0]], d, d0;
     var d = pt;
     d0 = geo.transform.transformCoordinates("EPSG:4326",
-               "EPSG:3857", d)
+               "EPSG:3857", d);
     expect(d0[0][0]).toBeCloseTo(0.0, 0);
     expect(d0[0][1]).toBeCloseTo(180.0, 0);
     expect(d0[1][0]).toBeCloseTo(0.0, 0);
@@ -46,7 +55,7 @@ describe('coordinate conversion', function () {
     var pt = [{ x: 0.0, y: 90.0 }, { x:0.0, y:0.0 }], d, d0;
     var d = pt;
     d0 = geo.transform.transformCoordinates("EPSG:4326",
-               "EPSG:3857", d)
+               "EPSG:3857", d);
     expect(d0[0].x).toBeCloseTo(0.0, 0);
     expect(d0[0].y).toBeCloseTo(180.0, 0);
     expect(d0[1].x).toBeCloseTo(0.0, 0);
@@ -57,7 +66,7 @@ describe('coordinate conversion', function () {
     var pt = [geo.latlng(90.0, 0.0), geo.latlng(0.0, 0.0)], d, d0;
     var d = pt;
     d0 = geo.transform.transformCoordinates("EPSG:4326",
-               "EPSG:3857", d)
+               "EPSG:3857", d);
     expect(d0[0].x()).toBeCloseTo(0.0, 0);
     expect(d0[0].y()).toBeCloseTo(180.0, 0);
     expect(d0[1].x()).toBeCloseTo(0.0, 0);
