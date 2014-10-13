f7686893a08f91daa3365cbcc524b29eeacdb471
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-10-13 08:29:10 -0400

    adding a test for multiple maps

---
+++ testing/test-cases/selenium-tests/multipleMaps/testTwoMaps.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from selenium_test import FirefoxTest, ChromeTest,\
+    setUpModule, tearDownModule
+
+
+class osmBase(object):
+    testCase = ('multipleMaps',)
+    testRevision = 1
+
+    def loadPage(self):
+        self.resizeWindow(640, 960)
+        self.loadURL('multipleMaps/index.html')
+        self.wait()
+
+    def test_2map_draw(self):
+        testName = 'twoMapDraw'
+        self.loadPage()
+        self.screenshotTest(testName)
+
+
+class FirefoxOSM(osmBase, FirefoxTest):
+    testCase = osmBase.testCase + ('firefox',)
+
+
+class ChromeOSM(osmBase, ChromeTest):
+    testCase = osmBase.testCase + ('chrome',)
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
