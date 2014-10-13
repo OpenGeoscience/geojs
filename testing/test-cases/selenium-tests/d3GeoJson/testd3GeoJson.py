cfd3bf07ed128ef34cec73f901424bc6a064084e
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-10-13 15:26:38 -0400

    Fixing revision number of test images

--- testing/test-cases/selenium-tests/d3GeoJson/testd3GeoJson.py
+++ testing/test-cases/selenium-tests/d3GeoJson/testd3GeoJson.py
@@ -6,6 +6,7 @@ from selenium_test import FirefoxTest, ChromeTest,\
 
 class d3GeoJsonBase(object):
     testCase = ('d3GeoJson',)
+    testRevision = 1
 
     def loadPage(self):
         self.resizeWindow(640, 480)
@@ -21,12 +22,10 @@ class d3GeoJsonBase(object):
 
 class FirefoxOSM(d3GeoJsonBase, FirefoxTest):
     testCase = d3GeoJsonBase.testCase + ('firefox',)
-    testRevision = 3
 
 
 class ChromeOSM(d3GeoJsonBase, ChromeTest):
     testCase = d3GeoJsonBase.testCase + ('chrome',)
-    testRevision = 4
 
 
 if __name__ == '__main__':
