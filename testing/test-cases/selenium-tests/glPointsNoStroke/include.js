6016db05278d388dc9caf8708e9c0c8af24f5d74
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-10-17 12:54:02 -0400

    Some more tests for GL point feature

--- testing/test-cases/selenium-tests/glPointsNoStroke/include.js
+++ testing/test-cases/selenium-tests/glPointsNoStroke/include.js
@@ -9,8 +9,21 @@ window.startTest = function (done) {
       var layer = myMap.createLayer('feature');
       layer.createFeature('point')
         .data(citieslatlon)
-        .style('fill', function (d) {
+        .style('stroke', function (d) {
           return false;
+        })
+        .style('fillColor', function (d) {
+          if (d.lon < -100) {
+            return {r: 1.0, g: 0.0, b: 0.0};
+          }
+          return {r: 0.0, g: 0.0, b: 1.0};
+        })
+        .style('fillOpacity', function (d) {
+          if (d.lon < -100) {
+            return 0.5;
+          } else {
+            return 0.25;
+          }
         })
         .position(function (d) { return {x: d.lon, y: d.lat, z: d.elev}; });
       myMap.draw();
--- testing/test-cases/selenium-tests/glPointsTransparent/include.js
+++ testing/test-cases/selenium-tests/glPointsTransparent/include.js
@@ -9,8 +9,17 @@ window.startTest = function (done) {
       var layer = myMap.createLayer('feature');
       layer.createFeature('point')
         .data(citieslatlon)
-        .style('fill', function (d) {
-          return false;
+        .style('fillColor', function (d) {
+          return {r: 0.0, g: 0.0, b: 1.0};
+        })
+        .style('strokeColor', function (d) {
+          return {r: 1.0, g: 0.0, b: 0.0};
+        })
+        .style('fillOpacity', function (d) {
+          return 0.2;
+        })
+        .style('strokeOpacity', function (d) {
+          return 0.2;
         })
         .position(function (d) { return {x: d.lon, y: d.lat, z: d.elev}; });
       myMap.draw();
