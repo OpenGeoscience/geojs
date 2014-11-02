7a10d71ce0e43c6c151fa02348e15673d7d8eed4
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-02 09:01:50 -0500

    Adding infrastructure for building ui components

---
+++ src/ui/widget.js
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * @module geo
+ */
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * Create a new instance of class widget
+ *
+ * @class
+ * @returns {geo.widget}
+ */
+//////////////////////////////////////////////////////////////////////////////
+geo.widget = function (arg) {
+  'use strict';
+  if (!(this instanceof geo.widget)) {
+    return new geo.widget(arg);
+  }
+  geo.sceneObject.call(this, arg);
+
+  var m_this = this;
+
+  this._init = function () {
+    m_this.modified();
+  };
+
+  this._exit = function () {
+    m_this.children().forEach(function (child) {
+      child._exit();
+    });
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Create feature give a name
+   *
+   * @returns {geo.Feature} Will return a new feature
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._createFeature = function (featureName, arg) {
+
+    var newFeature = geo.createFeature(
+      featureName, m_this, m_this.renderer(), arg);
+
+    m_this.addChild(newFeature);
+    m_this.modified();
+    return newFeature;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Delete feature
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._deleteFeature = function (feature) {
+    m_this.removeChild(feature);
+
+    return m_this;
+  };
+};
+inherit(geo.widget, geo.sceneObject);
