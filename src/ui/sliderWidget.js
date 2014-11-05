fa5a23b99db7acd62e48bdac73575d4587b2bcce
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-05 18:30:49 -0500

    Styled zoom slider

--- src/ui/sliderWidget.js
+++ src/ui/sliderWidget.js
@@ -37,30 +37,59 @@ geo.sliderWidget = function (arg) {
   /* http://icomoon.io */
   /* CC BY 3.0 http://creativecommons.org/licenses/by/3.0/ */
   /* jshint -W101 */
-  m_plusIcon = 'M992.262 871.396l-242.552-206.294c-25.074-22.566-51.89-32.926-73.552-31.926 57.256-67.068 91.842-154.078 91.842-249.176 0-212.078-171.922-384-384-384-212.076 0-384 171.922-384 384s171.922 384 384 384c95.098 0 182.108-34.586 249.176-91.844-1 21.662 9.36 48.478 31.926 73.552l206.294 242.552c35.322 39.246 93.022 42.554 128.22 7.356s31.892-92.898-7.354-128.22zM384 640c-141.384 0-256-114.616-256-256s114.616-256 256-256 256 114.616 256 256-114.614 256-256 256zM448 192h-128v128h-128v128h128v128h128v-128h128v-128h-128z';
-  m_minusIcon = 'M992.262 871.396l-242.552-206.294c-25.074-22.566-51.89-32.926-73.552-31.926 57.256-67.068 91.842-154.078 91.842-249.176 0-212.078-171.922-384-384-384-212.076 0-384 171.922-384 384s171.922 384 384 384c95.098 0 182.108-34.586 249.176-91.844-1 21.662 9.36 48.478 31.926 73.552l206.294 242.552c35.322 39.246 93.022 42.554 128.22 7.356s31.892-92.898-7.354-128.22zM384 640c-141.384 0-256-114.616-256-256s114.616-256 256-256 256 114.616 256 256-114.614 256-256 256zM192 320h384v128h-384z';
+  m_plusIcon = 'M512 81.92c-237.568 0-430.080 192.614-430.080 430.080 0 237.568 192.563 430.080 430.080 430.080s430.080-192.563 430.080-430.080c0-237.517-192.563-430.080-430.080-430.080zM564.326 564.326v206.182h-104.653v-206.182h-206.234v-104.653h206.182v-206.234h104.704v206.182h206.182v104.704h-206.182z';
+  m_minusIcon = 'M512 81.92c-237.568 0-430.080 192.614-430.080 430.080 0 237.568 192.563 430.080 430.080 430.080s430.080-192.563 430.080-430.080c0-237.517-192.563-430.080-430.080-430.080zM770.56 459.674v104.704h-517.12v-104.704h517.12z';
   /* jshint +W101 */
 
   function put_icon(icon, base, cx, cy, size) {
+    var g = base.append('g');
+
+    // the scale factor
+    var s = size / 1024;
+
+    g.append('g')
+      .append('g')
+        .attr(
+          'transform',
+          'translate(' + cx + ',' + cy + ') scale(' + s + ') translate(-512,-512)'
+      )
+      .append('path')
+        .attr('d', icon)
+        .attr('class', 'geo-glyphicon');
+
+    return g;
   }
 
   this._init = function () {
     var svg = m_this.layer().renderer().canvas(),
         x0 = 40,
         y0 = 40 + m_width,
-        map = m_this.layer().map();
+        map = m_this.layer().map(),
+        main = svg;
 
     // create d3 scales for positioning
     // TODO: make customizable and responsive
     m_xscale = d3.scale.linear().domain([-4, 4]).range([x0, x0 + m_width]);
     m_yscale = d3.scale.linear().domain([0, 1]).range([y0, y0 + m_height]);
 
-    m_plus = svg.append('circle')
+    // Add a background
+    var back = svg
+      .append('rect')
+        .attr('rx', 20)
+        .attr('ry', 20)
+        .style('fill', 'gray')
+        .style('fill-opacity', 1)
+        .attr('filter', 'url(#geo-blur)');
+
+    svg = svg.append('g');
+
+    m_plus = svg.append('g');
+    m_plus.append('circle')
       .attr('cx', m_xscale(0))
       .attr('cy', m_yscale(0.0) - m_width + 2)
       .attr('r', m_width / 2)
       .style({
-        'fill': 'red',
+        'fill': 'white',
         'cursor': 'pointer'
       })
       .on('click', function () {
@@ -71,12 +100,23 @@ geo.sliderWidget = function (arg) {
         d3.event.stopPropagation();
       });
 
-    m_minus = svg.append('circle')
+    put_icon(
+      m_plusIcon,
+      m_plus,
+      m_xscale(0),
+      m_yscale(0) - m_width + 2,
+      m_width + 5
+    ).style('cursor', 'pointer')
+      .style('pointer-events', 'none');
+
+
+    m_minus = svg.append('g');
+    m_minus.append('circle')
       .attr('cx', m_xscale(0))
       .attr('cy', m_yscale(1.0) + m_width - 2)
       .attr('r', m_width / 2)
       .style({
-        'fill': 'blue',
+        'fill': 'white',
         'cursor': 'pointer'
       })
       .on('click', function () {
@@ -87,6 +127,15 @@ geo.sliderWidget = function (arg) {
         d3.event.stopPropagation();
       });
 
+    put_icon(
+      m_minusIcon,
+      m_minus,
+      m_xscale(0),
+      m_yscale(1) + m_width - 2,
+      m_width + 5
+    ).style('cursor', 'pointer')
+      .style('pointer-events', 'none');
+
     m_track = svg.append('rect')
       .attr('x', m_xscale(0) - m_width / 10)
       .attr('y', m_yscale(0))
@@ -95,7 +144,8 @@ geo.sliderWidget = function (arg) {
       .attr('width', m_width / 5)
       .attr('height', m_height)
       .style({
-        'fill': 'lightgray',
+        'fill': 'white',
+        'stroke': 'black',
         'cursor': 'pointer'
       });
 
@@ -107,7 +157,7 @@ geo.sliderWidget = function (arg) {
       .attr('width', m_width)
       .attr('height', m_nubSize)
       .style({
-        'fill': 'darkgray',
+        'fill': 'black',
         'cursor': 'pointer'
       })
       .on('mousedown', function () {
@@ -128,6 +178,30 @@ geo.sliderWidget = function (arg) {
         });
         d3.event.stopPropagation();
       });
+
+    var bbox = svg.node().getBBox();
+    back.attr('width', bbox.width + 20)
+      .attr('height', bbox.height + 20)
+      .attr('x', bbox.x - 10)
+      .attr('y', bbox.y - 10)
+      .style('opacity', 1e-6);
+    main
+      .on('mouseenter', function () {
+        back.style('opacity', 1);
+      })
+      .on('mouseleave', function () {
+        back.style('opacity', 1e-6);
+      });
+
+
+    svg.selectAll('*')
+      .on('mouseover', function () {
+        d3.select(this).attr('filter', 'url(#geo-highlight)');
+      })
+      .on('mouseout', function () {
+        d3.select(this).attr('filter', null);
+      });
+
 
     m_this.layer().geoOn(geo.event.zoom, function () {
       m_this._update();
