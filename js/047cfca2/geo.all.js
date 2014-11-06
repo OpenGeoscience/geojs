ee0c502c4ed3c6ed7dd4bbb4a43a2e8098c125fb
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-06 08:13:15 -0500

    Added a ui example

---
+++ js/047cfca2/geo.all.js
@@ -0,0 +1,7 @@
+/**
+ *  Including this file will load geojs and it's dependencies.
+ */
+(function () {
+  document.write('<script src="/js/047cfca2/geo.ext.min.js" type="text/javascript" charset="UTF8"></script>');
+  document.write('<script src="/js/047cfca2/geo.min.js"></script>');
+}());
