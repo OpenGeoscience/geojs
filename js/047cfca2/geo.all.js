6a65c257099ab45d5ef0a71cf24e158ee06fdca4
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-06 08:14:48 -0500

    corrected library path

--- js/047cfca2/geo.all.js
+++ js/047cfca2/geo.all.js
@@ -2,6 +2,6 @@
  *  Including this file will load geojs and it's dependencies.
  */
 (function () {
-  document.write('<script src="/js/047cfca2/geo.ext.min.js" type="text/javascript" charset="UTF8"></script>');
-  document.write('<script src="/js/047cfca2/geo.min.js"></script>');
+  document.write('<script src="/geojs/js/047cfca2/geo.ext.min.js" type="text/javascript" charset="UTF8"></script>');
+  document.write('<script src="/geojs//js/047cfca2/geo.min.js"></script>');
 }());
