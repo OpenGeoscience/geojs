0c4060113e983159f89a6e2312f65da880bde5cf
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-07 08:18:49 -0500

    Added a contrast transition in slider

--- src/ui/sliderWidget.js
+++ src/ui/sliderWidget.js
@@ -33,7 +33,9 @@ geo.sliderWidget = function (arg) {
       m_nubSize = 10,
       m_plusIcon,
       m_minusIcon,
-      m_group;
+      m_group,
+      m_lowContrast,
+      m_highlightDur = 100;
 
   /* http://icomoon.io */
   /* CC BY 3.0 http://creativecommons.org/licenses/by/3.0/ */
@@ -41,6 +43,12 @@ geo.sliderWidget = function (arg) {
   m_plusIcon = 'M512 81.92c-237.568 0-430.080 192.614-430.080 430.080 0 237.568 192.563 430.080 430.080 430.080s430.080-192.563 430.080-430.080c0-237.517-192.563-430.080-430.080-430.080zM564.326 564.326v206.182h-104.653v-206.182h-206.234v-104.653h206.182v-206.234h104.704v206.182h206.182v104.704h-206.182z';
   m_minusIcon = 'M512 81.92c-237.568 0-430.080 192.614-430.080 430.080 0 237.568 192.563 430.080 430.080 430.080s430.080-192.563 430.080-430.080c0-237.517-192.563-430.080-430.080-430.080zM770.56 459.674v104.704h-517.12v-104.704h517.12z';
   /* jshint +W101 */
+
+  // Define off-white gray colors for low contrast ui (unselected).
+  m_lowContrast = {
+    white: '#f4f4f4',
+    black: '#505050'
+  };
 
 //////////////////////////////////////////////////////////////////////////////
 /**
@@ -102,12 +110,15 @@ geo.sliderWidget = function (arg) {
     // Create + zoom button
     m_plus = svg.append('g');
     m_plus.append('circle')
+      .datum({
+        fill: 'white',
+        stroke: null
+      })
       .classed('geo-zoom-in', true)
       .attr('cx', m_xscale(0))
       .attr('cy', m_yscale(0.0) - m_width + 2)
       .attr('r', m_width / 2)
       .style({
-        'fill': 'white',
         'cursor': 'pointer'
       })
       .on('click', function () {
@@ -129,18 +140,26 @@ geo.sliderWidget = function (arg) {
       m_yscale(0) - m_width + 2,
       m_width + 5
     ).style('cursor', 'pointer')
-      .style('pointer-events', 'none');
+      .style('pointer-events', 'none')
+      .select('path')
+      .datum({
+        fill: 'black',
+        stroke: null
+      });
 
 
     // Create the - zoom button
     m_minus = svg.append('g');
     m_minus.append('circle')
+      .datum({
+        fill: 'white',
+        stroke: null
+      })
       .classed('geo-zoom-out', true)
       .attr('cx', m_xscale(0))
       .attr('cy', m_yscale(1.0) + m_width - 2)
       .attr('r', m_width / 2)
       .style({
-        'fill': 'white',
         'cursor': 'pointer'
       })
       .on('click', function () {
@@ -162,7 +181,12 @@ geo.sliderWidget = function (arg) {
       m_yscale(1) + m_width - 2,
       m_width + 5
     ).style('cursor', 'pointer')
-      .style('pointer-events', 'none');
+      .style('pointer-events', 'none')
+      .select('path')
+      .datum({
+        fill: 'black',
+        stroke: null
+      });
 
     // Respond to a mouse event on the widget
     function respond(evt, trans) {
@@ -183,6 +207,10 @@ geo.sliderWidget = function (arg) {
 
     // Create the track
     m_track = svg.append('rect')
+      .datum({
+        fill: 'white',
+        stroke: 'black'
+      })
       .classed('geo-zoom-track', true)
       .attr('x', m_xscale(0) - m_width / 6)
       .attr('y', m_yscale(0))
@@ -191,8 +219,6 @@ geo.sliderWidget = function (arg) {
       .attr('width', m_width / 3)
       .attr('height', m_height)
       .style({
-        'fill': 'white',
-        'stroke': 'black',
         'cursor': 'pointer'
       })
       .on('click', function () {
@@ -201,6 +227,10 @@ geo.sliderWidget = function (arg) {
 
     // Create the nub
     m_nub = svg.append('rect')
+      .datum({
+        fill: 'black',
+        stroke: null
+      })
       .classed('geo-zoom-nub', true)
       .attr('x', m_xscale(-4))
       .attr('y', m_yscale(0.5) - m_nubSize / 2)
@@ -209,7 +239,6 @@ geo.sliderWidget = function (arg) {
       .attr('width', m_width)
       .attr('height', m_nubSize)
       .style({
-        'fill': 'black',
         'cursor': 'pointer'
       })
       .on('mousedown', function () {
@@ -223,21 +252,41 @@ geo.sliderWidget = function (arg) {
         d3.event.stopPropagation();
       });
 
+    var mouseOver = function () {
+      d3.select(this).attr('filter', 'url(#geo-highlight)');
+      m_group.selectAll('rect,path,circle').transition()
+        .duration(m_highlightDur)
+        .style('fill', function (d) {
+          return d.fill || null;
+        })
+        .style('stroke', function (d) {
+          return d.stroke || null;
+        });
 
-    // Apply a highlight effect
-    svg.selectAll('*')
-      .on('mouseover', function () {
-        d3.select(this).attr('filter', 'url(#geo-highlight)');
-      })
-      .on('mouseout', function () {
-        d3.select(this).attr('filter', null);
-      });
+    };
+
+    var mouseOut = function () {
+      d3.select(this).attr('filter', null);
+      m_group.selectAll('circle,rect,path').transition()
+        .duration(m_highlightDur)
+        .style('fill', function (d) {
+          return m_lowContrast[d.fill] || null;
+        })
+        .style('stroke', function (d) {
+          return m_lowContrast[d.stroke] || null;
+        });
+    };
+
+    m_group.selectAll('*')
+      .on('mouseover', mouseOver)
+      .on('mouseout', mouseOut);
 
     // Update the nub position on zoom
     m_this.layer().geoOn(geo.event.zoom, function () {
       m_this._update();
     });
 
+    mouseOut();
     m_this._update();
   };
 
