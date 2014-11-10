17f4048a39801dd3d23a23093107c1aa9abd881a
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-10 14:43:11 -0500

    checkpoint

--- examples/layers/main.js
+++ examples/layers/main.js
@@ -2,6 +2,19 @@
 $(function () {
   'use strict';
 
+  // Create an svg layer and return a reference to its dom element.
+  function getSvgLayer(map, opts) {
+
+    opts.renderer = 'd3Renderer';
+    var layer = map.createLayer('feature', opts);
+
+    return {
+      layer: layer,
+      svg: layer.canvas()
+    };
+  }
+
+
   // Create a map object
   var map = geo.map({
     node: '#map',
@@ -14,6 +27,8 @@ $(function () {
 
   // Add a base layer
   map.createLayer('osm');
+
+
 
   // Make the map resize with the browser window
   $(window).resize(function () {
