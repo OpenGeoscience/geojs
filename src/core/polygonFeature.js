22fd940be2e1ecd92288ece7d56fe362cf2ac542
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-11-21 22:18:44 -0500

    Add support for polygon feature

--- src/core/polygonFeature.js
+++ src/core/polygonFeature.js
@@ -20,8 +20,67 @@ geo.polygonFeature = function (arg) {
   arg = arg || {};
   geo.feature.call(this, arg);
 
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
   var m_this = this,
+      m_position,
+      m_polygons,
       s_init = this._init;
+
+  if (arg.line === undefined) {
+    m_polygons = function (d) {
+      return d;
+    };
+  } else {
+    m_polygons = arg.line;
+  }
+
+  if (arg.position === undefined) {
+    m_position = function (d) {
+      return d;
+    };
+  } else {
+    m_position = arg.position;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Get/Set line accessor
+   *
+   * @returns {geo.pointFeature}
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this.polygons = function (val) {
+    if (val === undefined) {
+      return m_polygons;
+    } else {
+      m_polygons = val;
+      m_this.dataTime().modified();
+      m_this.modified();
+    }
+    return m_this;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Get/Set position accessor
+   *
+   * @returns {geo.pointFeature}
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this.position = function (val) {
+    if (val === undefined) {
+      return m_position;
+    } else {
+      m_position = val;
+      m_this.dataTime().modified();
+      m_this.modified();
+    }
+    return m_this;
+  };
 
   ////////////////////////////////////////////////////////////////////////////
   /**
@@ -30,21 +89,28 @@ geo.polygonFeature = function (arg) {
   ////////////////////////////////////////////////////////////////////////////
   this._init = function (arg) {
     s_init.call(m_this, arg);
+
     var defaultStyle = $.extend(
       {},
       {
-        "color": [1.0, 1.0, 1.0],
-        "fill_color": [1.0, 1.0, 1.0],
-        "fill": true
+        "strokeWidth": 1.0,
+        // Default to gold color for lines
+        "strokeColor": { r: 1.0, g: 0.8431372549, b: 0.0 },
+        "strokeStyle": "solid",
+        "strokeOpacity": 1.0
       },
       arg.style === undefined ? {} : arg.style
     );
 
     m_this.style(defaultStyle);
+
+    if (m_position) {
+      m_this.dataTime().modified();
+    }
   };
 
-  m_this._init(arg);
-  return m_this;
+  this._init(arg);
+  return this;
 };
 
 inherit(geo.polygonFeature, geo.feature);
