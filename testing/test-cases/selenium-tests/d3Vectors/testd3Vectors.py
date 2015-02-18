a6d8f4835021d25c8740581fa6a791cec35afc17
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-02-18 07:38:50 -0500

    Increment d3Vectors test case

--- testing/test-cases/selenium-tests/d3Vectors/testd3Vectors.py
+++ testing/test-cases/selenium-tests/d3Vectors/testd3Vectors.py
@@ -6,7 +6,7 @@ from selenium_test import FirefoxTest, ChromeTest,\
 
 class d3VectorsBase(object):
     testCase = ('d3Vectors',)
-    testRevision = 5
+    testRevision = 6
 
     def loadPage(self):
         self.resizeWindow(640, 480)
