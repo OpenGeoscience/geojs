16a1f8e940df397d41bbce2bac92d977d6ff9885
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-07-22 02:30:43 -0400

    [WIP] Fixing bugs with positioning map

--- examples/points/main.js
+++ examples/points/main.js
@@ -2,108 +2,113 @@
 $(function () {
   'use strict';
 
-  // Define a function we will use to generate points.
-  function makePoints(data, layer, color) {
+  function draw_map() {
+
+  var map;
+
+  $(function () {
+
+    map = geo.map({node: '#map', center: {x: -70, y: 30}, zoom: 0});
+    map.createLayer('osm');
+    var ui = map.createLayer('ui');
+    var legend = ui.createWidget('legend');
 
 
-    // The API for creating features is similar to d3's data API.
-    // The data is an array of arbitrary objects.  Each object in
-    // the array is assumed to be a "point".  You provide accessors
-    // that return styles and positions.
-    // Here we also use the option `selectionAPI` to turn on the
-    // optional mouse handling API.
-    return layer.createFeature('point', {selectionAPI: true})
-      .data(data)                                               // bind data
-        .position(function (d) { return {x: d.x, y: d.y}; })    // position accessor
-        .style('radius', 100)                                   // circle radius
-        .style('fillColor', color)
-        .style('fillOpacity', function (d) { return d.opacity; })
-        .style('strokeColor', {r: 0, g: 0, b: 0})
-        .style('strokeWidth', 2)
+    var data = [{x: -70, y: 30}];
 
-        // Here we bind mouse event handlers to the markers.  In this case,
-        // we are increasing the opacity of the feature on mouse hover.
-        // You are given access to the original data as `evt.data`,
-        // the index in the data as `evt.index`, as well as all
-        // information given by `geo.event.mousemove` events.
-        .geoOn(geo.event.feature.mouseover, function (evt) {
-          evt.data.opacity = 0.5;
-          this.modified();        // mark the feature as modified
-          layer.map().draw();     // we must redraw as necessary
-        })
-        .geoOn(geo.event.feature.mouseout, function (evt) {
-          evt.data.opacity = 0.1;
-          this.modified();
-          layer.map().draw();
-        })
+    var point_gl0 = map.createLayer('feature', {'render': 'vgl'}).createFeature('point'),
+        point_d30 = map.createLayer('feature', {'render': 'd3'}).createFeature('point'),
+        point_gl1 = null,
+        point_d31 = null;
+    // Reove this line to watch the points get offset
+    map.bounds(
+      { lowerLeft:
+          {x:-100, y:0 },
+        upperRight:
+          {x:-60, y:40} }
+    );
 
-        // You must call the draw method after setting all feature
-        // properties.
-        .draw();
-  }
+    var gl0_style = {
+      'radius': 12,
+      'strokeColor': 'steelblue',
+      'strokeWidth': 2,
+      'fillColor': 'steelblue',
+      'fillOpacity': 0.25
+    };
 
-  // Create a map object with the OpenStreetMaps base layer.
-  var map = geo.map({
-    node: '#map',
-    center: {
-      x: -98.0,
-      y: 39.5
-    },
-    zoom: 2
-  });
+    var d30_style = {
+      'radius': 12,
+      'strokeColor': 'firebrick',
+      'strokeWidth': 2,
+      'fillColor': 'firebrick',
+      'fillOpacity': 0.25
+    };
 
-  // Create an osm layer with custom tile url for a white background.
-  map.createLayer(
-    'osm',
-    {
-      tileUrl: function () {
-        return 'white.jpg';
+   var gl1_style = {
+      'radius': 6,
+      'stroke': false,
+      'fillColor': 'blue',
+      'fillOpacity': 1
+    };
+
+    var d31_style = {
+      'radius': 6,
+      'stroke': false,
+      'fillColor': 'red',
+      'fillOpacity': 1
+    };
+
+    legend.categories([
+      {name: 'gl reference', style: gl0_style, type: 'point'},
+      {name: 'd3 reference', style: d30_style, type: 'point'},
+      {name: 'gl delayed', style: gl1_style, type: 'point'},
+      {name: 'd3 delayed', style: d31_style, type: 'point'}
+    ]);
+
+    point_gl0
+      .style(gl0_style)
+      .data([{x: -75, y: 30}]);
+
+    point_d30
+      .style(d30_style)
+      .data([{x: -65, y: 30}]);
+
+    map.draw();
+
+    // draw points
+    function draw_points() {
+      point_gl1 = map.createLayer('feature', {'render': 'vgl'}).createFeature('point');
+      point_d31 = map.createLayer('feature', {'render': 'd3'}).createFeature('point');
+
+      point_gl1
+        .style(gl1_style)
+        .data([{x: -75, y: 30}]);
+
+      point_d31
+        .style(d31_style)
+        .data([{x: -65, y: 30}]);
+
+      map.draw();
+    }
+
+    function draw_points_after_2seconds() {
+
+      var i = 0;
+      function tick() {
+        i += 1;
+        $('#time-left').val((2 - i / 100).toFixed(2));
+        if (i < 200) {
+          window.setTimeout(tick, 10);
+        } else {
+          draw_points();
+        }
       }
+      tick();
     }
-  );
 
-  // Create a gl feature layer
-  var vglLayer = map.createLayer(
-    'feature',
-    {
-      renderer: 'vgl'
-    }
-  );
-
-  // Create an svg feature layer
-  var svgLayer = map.createLayer(
-    'feature',
-    {
-      renderer: 'd3'
-    }
-  );
-
-  // Define unique colors for each layer
-  var vglColor = 'red';
-  var svgColor = 'blue';
-
-  // Generate some data for vgl
-  var data = d3.range(2).map(function (i) {
-    return {
-      x: -95,             // longitude
-      y: 39.5 + 4.5 * i,  // latitude
-      c: vglColor,        // fill color
-      opacity: 0.1        // fill opacity
-    };
+    $('#add-point').click(draw_points_after_2seconds);
   });
-  makePoints(data, vglLayer, vglColor);
+}
 
-  // Generate some data for svg
-  data = d3.range(2).map(function (i) {
-    return {
-      x: -101,            // longitude
-      y: 39.5 + 4.5 * i,  // latitude
-      c: svgColor,        // fill color
-      opacity: 0.1        // fill opacity
-    };
-  });
-  makePoints(data, svgLayer, svgColor);
-
-  // Draw the map
-  map.draw();
+draw_map();
 });
