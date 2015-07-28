944e3e9ac3e4f78e0cb99f8015f7f14b08fab741
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-07-28 08:53:46 -0400

    Adding a test case for tileCache

--- src/core/tileCache.js
+++ src/core/tileCache.js
@@ -53,28 +53,96 @@
     },
     set size(n) {
       var i;
-      for (i = n; i < this._size; i += 1) {
-        this._remove(i);
+      for (i = n; i < this.length; i += 1) {
+        this.remove(this._atime[i]);
       }
       this._size = n;
     },
 
     /**
-     * Remove a tile from the cache.
-     * @param {Number} index The position in the queue to remove
+     * Get the current cache size.
      */
-    _remove: function (index) {
-      var tile = this._atime[index];
+    get length() {
+      return this._atime.length;
+    },
+
+    /**
+     * Get the position of the tile in the access queue.
+     * @param {string} hash The tile's hash value
+     * @returns {Number} The position in the queue or -1
+     */
+    _access: function (tile) {
+      return this._atime.indexOf(tile);
+    },
+
+    /**
+     * Remove a tile from the cache.
+     * @param {string|geo.tile} tile The tile or its hash
+     * @returns {bool} true if a tile was removed
+     */
+    remove: function (tile) {
+      if (typeof tile !== 'string') {
+        tile = this.hash(tile);
+      }
+
+      // if the tile is not in the cache
+      if (!(tile in this._cache)) {
+        return false;
+      }
+
+      // Remove the tile from the access queue
+      this._atime.splice(this._access(tile), 1);
+
+      // Remove the tile from the cache
       delete this._cache[tile];
+      return true;
     },
 
     /**
      * Remove all tiles from the cache.
      */
     clear: function () {
-      this._cache = {};
-      this._atime = [];
-      this._index = 0;
+      this._cache = {};  // The hash -> tile mapping
+      this._atime = [];  // The access queue
+      return this;
+    },
+
+    /**
+     * Get a tile from the cache if it exists, otherwise
+     * return null.  This method also moves the tile to the
+     * front of the access queue.
+     *
+     * @param {string} hash The tile hash value
+     * @returns {geo.tile|null}
+     */
+    get: function (hash) {
+      if (!(hash in this._cache)) {
+        return null;
+      }
+
+      this._atime.unshift(
+        this._atime.splice(this._access(hash), 1)
+      );
+      return this._cache[hash];
+    },
+
+    /**
+     * Add a tile to the cache.
+     * @param {geo.tile} tile
+     */
+    add: function (tile) {
+      // remove any existing tiles with the same hash
+      this.remove(tile);
+
+      // add the tile
+      this._cache[this.hash(tile)] = tile;
+      this._atime.unshift(tile);
+
+      // purge a tile from the cache if necessary
+      if (this._atime.length > this.size) {
+        tile = this._atime.pop();
+        delete this._cache[tile];
+      }
     }
   };
 })();
