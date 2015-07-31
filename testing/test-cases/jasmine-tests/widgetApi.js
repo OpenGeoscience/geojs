0955c18d2ed8eed2ad80ce8cb468d35a1acf4a2c
Dan LaManna <dan.lamanna@kitware.com>
2015-07-31 12:53:28 -0400

    Clean and fix widget API tests to pass

--- testing/test-cases/jasmine-tests/widgetApi.js
+++ testing/test-cases/jasmine-tests/widgetApi.js
@@ -1,4 +1,4 @@
-/*global describe, it, expect, geo, xit, jasmine*/
+/*global describe, it, expect, geo, jasmine*/
 
 describe('widget api', function () {
   'use strict';
@@ -20,91 +20,94 @@ describe('widget api', function () {
   map.draw();
 
   var uiLayer = map.createLayer('ui');
-  var widgets = [];
+  window.geoWidgets = [];
 
-  describe('a widget stuck to albany', function () {
-    widgets.push(uiLayer.createWidget('dom', {
+  it('a widget should have the UI layer as its parent', function (done) {
+    var widget = uiLayer.createWidget('dom');
+
+    // @todo this is kind of ugly
+    setTimeout(function () {
+      done();
+    }, 1000);
+
+    expect(widget.parent()).toEqual(jasmine.any(geo.gui.uiLayer));
+  });
+
+  it('a widget stuck to albany shouldn\'t be in the viewport ' +
+     'if we pan to moscow', function (done) {
+       var widget = uiLayer.createWidget('dom', {
+         sticky: true,
+         positionType: 'gcs',
+         position: {
+           x: -73.7572,
+           y: 42.6525
+         }
+       });
+
+       map.transition({
+         center: {x: 37.6167, y: 55.7500}
+       });
+
+       map.geoOff(geo.event.transitionend)
+         .geoOn(geo.event.transitionend, function () {
+           expect(widget.isInViewport()).toBe(false);
+           done();
+         });
+     });
+
+  it('a widget stuck to albany should be in the viewport if albany is', function (done) {
+    var widget = uiLayer.createWidget('dom', {
       sticky: true,
       positionType: 'gcs',
       position: {
         x: -73.7572,
         y: 42.6525
       }
-    }));
-
-    var widget = widgets[0];
-
-    it('should be the only child of the ui layer', function () {
-      expect($(uiLayer.canvas()).children().length).toBe(1);
-      expect($(uiLayer.canvas()).children()[0].tagName).toBe('DIV');
     });
 
-    it('should have the UI layer as it\'s parent', function () {
-      expect(widget.parent).toEqual(jasmine.any(geo.gui.uiLayer));
+    map.transition({
+      center: {x: -73.7572, y: 42.6525}
     });
 
-    it('should be out of the viewport if we pan to moscow', function () {
-      map.transition({
-        center: {x: 37.6167, y: 55.7500}
+    map.geoOff(geo.event.transitionend)
+      .geoOn(geo.event.transitionend, function () {
+        expect(widget.isInViewport()).toBe(true);
+        done();
       });
-
-      expect(widget.isInViewport()).toBe(false);
-    });
-
-    it('should be back in the viewport if we pan to albany', function () {
-      map.transition({
-        center: {x: 37.6167, y: 55.7500}
-      });
-
-      expect(widget.isInViewport()).toBe(true);
-    });
   });
 
-  describe('a sticky widget on the top left', function () {
-    widgets.push(uiLayer.createWidget('dom', {
+  it('a widget stuck to the top left should always be in the viewport', function (done) {
+    var widget = uiLayer.createWidget('dom', {
       sticky: true
-    }));
+    });
 
-    var widget = widgets[1];
+    expect($(widget.canvas()).position()).toEqual({top: 0, left: 0});
 
-    it('should always be in the viewport', function () {
-      expect($(widget.canvas()).position()).toBe({top: 0, left: 0});
-      expect(widget.isInViewport()).toBe(true);
+    map.transition({
+      center: {x: 37.6167, y: 55.7500}
+    });
 
-      map.transition({
-        center: {x: 37.6167, y: 55.7500}
+    map.geoOff(geo.event.transitionend)
+      .geoOn(geo.event.transitionend, function () {
+        expect(widget.isInViewport()).toBe(true);
+        expect($(widget.canvas()).position()).toEqual({top: 0, left: 0});
+        done();
       });
-
-      expect(widget.isInViewport()).toBe(true);
-      expect($(widget.canvas()).position()).toBe({top: 0, left: 0});
-    });
   });
 
-  describe('removing the widgets', function () {
-    widgets.map(function (widget) {
-      widget._exit();
-    });
-
-    it('should leave the ui layer empty', function () {
-      expect($(uiLayer.canvas()).children().length).toBe(0);
-    });
-  });
-
-
-  describe('an svg widget within a dom widget', function () {
+  it('nested widgets should be properly structured', function () {
     var domWidget = uiLayer.createWidget('dom');
     var svgWidget = uiLayer.createWidget('svg', {
       parent: domWidget
     });
+    var widgetCount = $(uiLayer.canvas()).children().length;
 
-    it('should be properly structured', function () {
-      expect($(uiLayer.canvas()).children().length).toBe(1);
-      expect($(svgWidget.canvas()).parent()[0]).toBe($(domWidget.canvas())[0]);
-    });
+    expect($(svgWidget.canvas()).parent()[0]).toBe($(domWidget.canvas())[0]);
 
-    it('should be removed when its parent is', function () {
-      domWidget._exit();
-      expect($(uiLayer.canvas()).children().length).toBe(0);
-    });
+    // Only top level widgets are children of the UI Layer
+    // So removing the domWidget will also remove the svgWidget, but only
+    // reduce the number of widgets the UILayer has as children by 1
+    domWidget._exit();
+    expect($(uiLayer.canvas()).children().length).toBe(widgetCount - 1);
   });
 });
