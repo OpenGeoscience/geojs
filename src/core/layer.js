1df3f45392ff0b3ade4c0e5f205be6d5fda92578
Dan LaManna <dan.lamanna@kitware.com>
2015-08-05 10:46:03 -0400

    Move uiLayer logic out of layer.js

--- src/core/layer.js
+++ src/core/layer.js
@@ -376,11 +376,7 @@ geo.layer = function (arg) {
     // TODO: need to position according to offsets from the map element
     //       and maybe respond to events in case the map element moves
     //       around the page.
-    if (m_this instanceof geo.gui.uiLayer) {
-      m_node.css("position", "relative");
-    } else {
-      m_node.css("position", "absolute");
-    }
+    m_node.css("position", "absolute");
 
     if (m_map) {
       m_map.node().append(m_node);
