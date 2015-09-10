b171f0bfb7c8ea251ac9c5acee5383d087be3c44
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-09-10 08:12:27 -0400

    Test tileLayer on a map centered away from 0

--- testing/test-cases/phantomjs-tests/tileLayer.js
+++ testing/test-cases/phantomjs-tests/tileLayer.js
@@ -57,66 +57,132 @@ describe('geo.tileLayer', function () {
   };
 
   describe('tileAtPoint', function () {
-    var opts = {},
-        m = map(opts),
-        l = geo.tileLayer({map: m}),
-        s = m.unitsPerPixel();
+    describe('center: (0, 0)', function () {
+      var opts = {},
+          m = map(opts),
+          l = geo.tileLayer({map: m}),
+          s = m.unitsPerPixel();
 
-    it('origin: (0, 0), zoom level: 0', function () {
-      m.origin({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 128 * s, y: 128 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 64 * s, y: 192 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 192 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 192 * s, y: 192 * s}, 0)).toEqual({x: 0, y: 0});
+      it('origin: (0, 0), zoom level: 0', function () {
+        m.origin({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 128 * s, y: 128 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 192 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 192 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 192 * s, y: 192 * s}, 0)).toEqual({x: 0, y: 0});
+      });
+
+      it('origin: (0, 0), zoom level: 1', function () {
+        m.origin({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 1)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 192 * s}, 1)).toEqual({x: 0, y: 1});
+        expect(l.tileAtPoint({x: 192 * s, y: 64 * s}, 1)).toEqual({x: 1, y: 0});
+        expect(l.tileAtPoint({x: 192 * s, y: 192 * s}, 1)).toEqual({x: 1, y: 1});
+      });
+
+      it('origin: (0, 0), zoom level: 18', function () {
+        var t = Math.pow(2, -19) * 256;
+        m.origin({x: 0, y: 0});
+        expect(l.tileAtPoint({x: t * s, y: t * s}, 18)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: t * s, y: 3 * t * s}, 18)).toEqual({x: 0, y: 1});
+        expect(l.tileAtPoint({x: 3 * t * s, y: t * s}, 18)).toEqual({x: 1, y: 0});
+        expect(l.tileAtPoint({x: 3 * t * s, y: 3 * t * s}, 18)).toEqual({x: 1, y: 1});
+        expect(l.tileAtPoint({x: 13 * t * s, y: 7 * t * s}, 18)).toEqual({x: 6, y: 3});
+      });
+
+      it('origin: (128, 128), zoom level: 0', function () {
+        m.origin({x: 128 * s, y: 128 * s});
+        expect(l.tileAtPoint({x: 0 * s, y: 0 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: -64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: -64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: -64 * s, y: -64 * s}, 0)).toEqual({x: 0, y: 0});
+      });
+
+      it('origin: (128, 128), zoom level: 1', function () {
+        m.origin({x: 128 * s, y: 128 * s});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 1)).toEqual({x: 1, y: 1});
+        expect(l.tileAtPoint({x: 64 * s, y: -64 * s}, 1)).toEqual({x: 1, y: 0});
+        expect(l.tileAtPoint({x: -64 * s, y: 64 * s}, 1)).toEqual({x: 0, y: 1});
+        expect(l.tileAtPoint({x: -64 * s, y: -64 * s}, 1)).toEqual({x: 0, y: 0});
+      });
+
+      it('origin: (128, 128), zoom level: 18', function () {
+        var t = Math.pow(2, -19) * 256,
+            c = Math.pow(2, 17);
+        m.origin({x: 128 * s, y: 128 * s});
+        expect(l.tileAtPoint({x: t * s, y: t * s}, 18)).toEqual({x: c, y: c});
+        expect(l.tileAtPoint({x: t * s, y: 3 * t * s}, 18)).toEqual({x: c, y: c + 1});
+        expect(l.tileAtPoint({x: 3 * t * s, y: t * s}, 18)).toEqual({x: c + 1, y: c});
+        expect(l.tileAtPoint({x: 3 * t * s, y: 3 * t * s}, 18))
+          .toEqual({x: c + 1, y: c + 1});
+        expect(l.tileAtPoint({x: 13 * t * s, y: 7 * t * s}, 18))
+          .toEqual({x: c + 6, y: c + 3});
+      });
     });
+    describe('center: (1, -2)', function () {
+      var opts = {},
+          m = map(opts),
+          l = geo.tileLayer({map: m}),
+          s = m.unitsPerPixel();
 
-    it('origin: (0, 0), zoom level: 1', function () {
-      m.origin({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 1)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 64 * s, y: 192 * s}, 1)).toEqual({x: 0, y: 1});
-      expect(l.tileAtPoint({x: 192 * s, y: 64 * s}, 1)).toEqual({x: 1, y: 0});
-      expect(l.tileAtPoint({x: 192 * s, y: 192 * s}, 1)).toEqual({x: 1, y: 1});
-    });
+      m.center({x: 1 * s, y: -2 * s});
+      it('origin: (0, 0), zoom level: 0', function () {
+        m.origin({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 128 * s, y: 128 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 192 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 192 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 192 * s, y: 192 * s}, 0)).toEqual({x: 0, y: 0});
+      });
 
-    it('origin: (0, 0), zoom level: 18', function () {
-      var t = Math.pow(2, -19) * 256;
-      m.origin({x: 0, y: 0});
-      expect(l.tileAtPoint({x: t * s, y: t * s}, 18)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: t * s, y: 3 * t * s}, 18)).toEqual({x: 0, y: 1});
-      expect(l.tileAtPoint({x: 3 * t * s, y: t * s}, 18)).toEqual({x: 1, y: 0});
-      expect(l.tileAtPoint({x: 3 * t * s, y: 3 * t * s}, 18)).toEqual({x: 1, y: 1});
-      expect(l.tileAtPoint({x: 13 * t * s, y: 7 * t * s}, 18)).toEqual({x: 6, y: 3});
-    });
+      it('origin: (0, 0), zoom level: 1', function () {
+        m.origin({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 1)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 192 * s}, 1)).toEqual({x: 0, y: 1});
+        expect(l.tileAtPoint({x: 192 * s, y: 64 * s}, 1)).toEqual({x: 1, y: 0});
+        expect(l.tileAtPoint({x: 192 * s, y: 192 * s}, 1)).toEqual({x: 1, y: 1});
+      });
 
-    it('origin: (128, 128), zoom level: 0', function () {
-      m.origin({x: 128 * s, y: 128 * s});
-      expect(l.tileAtPoint({x: 0 * s, y: 0 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: 64 * s, y: -64 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: -64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
-      expect(l.tileAtPoint({x: -64 * s, y: -64 * s}, 0)).toEqual({x: 0, y: 0});
-    });
+      it('origin: (0, 0), zoom level: 18', function () {
+        var t = Math.pow(2, -19) * 256;
+        m.origin({x: 0, y: 0});
+        expect(l.tileAtPoint({x: t * s, y: t * s}, 18)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: t * s, y: 3 * t * s}, 18)).toEqual({x: 0, y: 1});
+        expect(l.tileAtPoint({x: 3 * t * s, y: t * s}, 18)).toEqual({x: 1, y: 0});
+        expect(l.tileAtPoint({x: 3 * t * s, y: 3 * t * s}, 18)).toEqual({x: 1, y: 1});
+        expect(l.tileAtPoint({x: 13 * t * s, y: 7 * t * s}, 18)).toEqual({x: 6, y: 3});
+      });
 
-    it('origin: (128, 128), zoom level: 1', function () {
-      m.origin({x: 128 * s, y: 128 * s});
-      expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 1)).toEqual({x: 1, y: 1});
-      expect(l.tileAtPoint({x: 64 * s, y: -64 * s}, 1)).toEqual({x: 1, y: 0});
-      expect(l.tileAtPoint({x: -64 * s, y: 64 * s}, 1)).toEqual({x: 0, y: 1});
-      expect(l.tileAtPoint({x: -64 * s, y: -64 * s}, 1)).toEqual({x: 0, y: 0});
-    });
+      it('origin: (128, 128), zoom level: 0', function () {
+        m.origin({x: 128 * s, y: 128 * s});
+        expect(l.tileAtPoint({x: 0 * s, y: 0 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: 64 * s, y: -64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: -64 * s, y: 64 * s}, 0)).toEqual({x: 0, y: 0});
+        expect(l.tileAtPoint({x: -64 * s, y: -64 * s}, 0)).toEqual({x: 0, y: 0});
+      });
 
-    it('origin: (128, 128), zoom level: 18', function () {
-      var t = Math.pow(2, -19) * 256,
-          c = Math.pow(2, 17);
-      m.origin({x: 128 * s, y: 128 * s});
-      expect(l.tileAtPoint({x: t * s, y: t * s}, 18)).toEqual({x: c, y: c});
-      expect(l.tileAtPoint({x: t * s, y: 3 * t * s}, 18)).toEqual({x: c, y: c + 1});
-      expect(l.tileAtPoint({x: 3 * t * s, y: t * s}, 18)).toEqual({x: c + 1, y: c});
-      expect(l.tileAtPoint({x: 3 * t * s, y: 3 * t * s}, 18))
-        .toEqual({x: c + 1, y: c + 1});
-      expect(l.tileAtPoint({x: 13 * t * s, y: 7 * t * s}, 18))
-        .toEqual({x: c + 6, y: c + 3});
+      it('origin: (128, 128), zoom level: 1', function () {
+        m.origin({x: 128 * s, y: 128 * s});
+        expect(l.tileAtPoint({x: 64 * s, y: 64 * s}, 1)).toEqual({x: 1, y: 1});
+        expect(l.tileAtPoint({x: 64 * s, y: -64 * s}, 1)).toEqual({x: 1, y: 0});
+        expect(l.tileAtPoint({x: -64 * s, y: 64 * s}, 1)).toEqual({x: 0, y: 1});
+        expect(l.tileAtPoint({x: -64 * s, y: -64 * s}, 1)).toEqual({x: 0, y: 0});
+      });
+
+      it('origin: (128, 128), zoom level: 18', function () {
+        var t = Math.pow(2, -19) * 256,
+            c = Math.pow(2, 17);
+        m.origin({x: 128 * s, y: 128 * s});
+        expect(l.tileAtPoint({x: t * s, y: t * s}, 18)).toEqual({x: c, y: c});
+        expect(l.tileAtPoint({x: t * s, y: 3 * t * s}, 18)).toEqual({x: c, y: c + 1});
+        expect(l.tileAtPoint({x: 3 * t * s, y: t * s}, 18)).toEqual({x: c + 1, y: c});
+        expect(l.tileAtPoint({x: 3 * t * s, y: 3 * t * s}, 18))
+          .toEqual({x: c + 1, y: c + 1});
+        expect(l.tileAtPoint({x: 13 * t * s, y: 7 * t * s}, 18))
+          .toEqual({x: c + 6, y: c + 3});
+      });
     });
   });
 });
