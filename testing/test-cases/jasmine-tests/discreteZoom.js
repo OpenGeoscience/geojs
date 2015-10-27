58e2e5ca3a322bc2d64a874a6e120b8d08f15325
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-10-27 10:05:20 -0400

    Fix jasmine tests with coverage turned on

--- testing/test-cases/jasmine-tests/discreteZoom.js
+++ testing/test-cases/jasmine-tests/discreteZoom.js
@@ -3,27 +3,36 @@
 describe('DiscreteZoom and ParallelProjection', function () {
   'use strict';
 
-  var map, width = 800, height = 800;
-  var cam;
+  function makeMap() {
+    var map, width = 800, height = 800;
+    var parent;
 
-  // create an osm map layer
-  map = geo.map({
-    'node': '#map',
-    'center': [0, 0],
-    'zoom': 3,
-    discreteZoom: true
-  });
-  map.createLayer('osm');
-  map.resize(0, 0, width, height);
-  map.draw();
+    parent = $('#map').parent();
+    $('#map').remove();
+    parent.append('<div id=map/>');
+
+    // create an osm map layer
+    map = geo.map({
+      'node': '#map',
+      'center': [0, 0],
+      'zoom': 3,
+      discreteZoom: true
+    });
+    map.createLayer('osm');
+    map.resize(0, 0, width, height);
+    map.draw();
+    return map;
+  }
 
   it('Zoom to a non-integer value', function () {
+    var map = makeMap();
     expect(map.discreteZoom()).toBe(true);
     map.zoom(5.75);
     expect(map.zoom()).toBe(6);
   });
 
   it('Turn off discrete zoom and zoom to a non-integer value', function () {
+    var map = makeMap();
     map.discreteZoom(false);
     expect(map.discreteZoom()).toBe(false);
     map.zoom(5.75);
@@ -31,12 +40,16 @@ describe('DiscreteZoom and ParallelProjection', function () {
   });
 
   it('Turn back on discrete zoom', function () {
+    var map = makeMap();
+    map.discreteZoom(false);
+    map.zoom(5.75);
     map.discreteZoom(true);
     expect(map.zoom()).toBe(6);
   });
 
   it('Turn on parallel projection', function () {
-    cam = map.baseLayer().renderer().contextRenderer().camera();
+    var map = makeMap();
+    var cam = map.baseLayer().renderer().contextRenderer().camera();
     var proj = cam.projectionMatrix();
     expect(proj[1] === 0 && proj[2] === 0 && proj[3] === 0 && proj[4] === 0 &&
            proj[6] === 0 && proj[7] === 0 && proj[8] === 0 && proj[9] === 0 &&
--- testing/test-cases/jasmine-tests/widgetApi.js
+++ testing/test-cases/jasmine-tests/widgetApi.js
@@ -3,78 +3,81 @@
 describe('widget api', function () {
   'use strict';
 
-  var map, width = 800, height = 800;
+  function makeMap() {
+    var map, width = 800, height = 800, parent;
 
-  map = geo.map({
-    node: '#map',
-    center: {
-      x: -98.0,
-      y: 39.5
-    },
-    zoom: 5
-  });
+    parent = $('#map').parent();
+    $('#map').remove();
+    parent.append('<div id=map/>');
 
-  map.createLayer('osm');
-  map.resize(0, 0, width, height);
-  map.createLayer('ui');
-  map.draw();
+    map = geo.map({
+      node: '#map',
+      center: {
+        x: -98.0,
+        y: 39.5
+      },
+      zoom: 5
+    });
 
-  var uiLayer = map.createLayer('ui');
-  window.geoWidgets = [];
+    map.createLayer('osm');
+    map.resize(0, 0, width, height);
+    map.createLayer('ui');
+    map.draw();
 
-  it('a widget should have the UI layer as its parent', function (done) {
-    var widget = uiLayer.createWidget('dom');
+    var uiLayer = map.createLayer('ui');
+    window.geoWidgets = [];
+    return {map: map, uiLayer: uiLayer};
+  }
 
-    // @todo this is kind of ugly
-    setTimeout(function () {
-      done();
-    }, 1000);
+  it('a widget should have the UI layer as its parent', function () {
+    var widget = makeMap().uiLayer.createWidget('dom');
 
     expect(widget.parent()).toEqual(jasmine.any(geo.gui.uiLayer));
   });
 
   it('a widget stuck to albany shouldn\'t be in the viewport ' +
      'if we pan to moscow', function () {
-       var widget = uiLayer.createWidget('dom', {
+       var o = makeMap(), widget = o.uiLayer.createWidget('dom', {
          position: {
            x: -73.7572,
            y: 42.6525
          }
        });
 
-       map.center({x: 37.6167, y: 55.7500});
+       o.map.center({x: 37.6167, y: 55.7500});
        expect(widget.isInViewport()).toBe(false);
      });
 
   it('a widget stuck to albany should be in the viewport if albany is', function () {
-    var widget = uiLayer.createWidget('dom', {
+    var o = makeMap(), widget = o.uiLayer.createWidget('dom', {
       position: {
         x: -73.7572,
         y: 42.6525
       }
     });
 
-    map.center({x: -73.7572, y: 42.6525});
+    o.map.center({x: -73.7572, y: 42.6525});
     expect(widget.isInViewport()).toBe(true);
 
   });
 
   it('a widget stuck to the top left should always be in the viewport', function () {
-    var widget = uiLayer.createWidget('dom');
+    var o = makeMap(), widget = o.uiLayer.createWidget('dom');
 
     expect($(widget.canvas()).position()).toEqual({top: 0, left: 0});
 
-    map.center({x: 37.6167, y: 55.7500});
+    o.map.center({x: 37.6167, y: 55.7500});
     expect(widget.isInViewport()).toBe(true);
     expect($(widget.canvas()).position()).toEqual({top: 0, left: 0});
   });
 
   it('nested widgets should be properly structured', function () {
-    var domWidget = uiLayer.createWidget('dom');
-    var svgWidget = uiLayer.createWidget('svg', {
+    var o = makeMap();
+    var domWidget = o.uiLayer.createWidget('dom');
+    var svgWidget = o.uiLayer.createWidget('svg', {
       parent: domWidget
     });
-    var widgetCount = $(uiLayer.canvas()).children().length;
+    var widgetCount = $(o.uiLayer.canvas()).children().length;
 
     expect($(svgWidget.canvas()).parent()[0]).toBe($(domWidget.canvas())[0]);
 
@@ -82,6 +85,6 @@ describe('widget api', function () {
     // So removing the domWidget will also remove the svgWidget, but only
     // reduce the number of widgets the UILayer has as children by 1
     domWidget._exit();
-    expect($(uiLayer.canvas()).children().length).toBe(widgetCount - 1);
+    expect($(o.uiLayer.canvas()).children().length).toBe(widgetCount - 1);
   });
 });
