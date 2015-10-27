35c75bc1c1b36a7f055ce3ca2dcdb0415e6418c9
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-10-27 09:18:58 -0400

    Fix discrete zoom test

--- testing/test-cases/jasmine-tests/discreteZoom.js
+++ testing/test-cases/jasmine-tests/discreteZoom.js
@@ -17,54 +17,39 @@ describe('DiscreteZoom and ParallelProjection', function () {
   map.resize(0, 0, width, height);
   map.draw();
 
-  it('Zoom to a non-integer value', function (done) {
+  it('Zoom to a non-integer value', function () {
     expect(map.discreteZoom()).toBe(true);
-    map.geoOff(geo.event.transitionend)
-      .geoOn(geo.event.transitionend, function () {
-        expect(map.zoom()).toBe(6);
-        done();
-      });
     map.zoom(5.75);
+    expect(map.zoom()).toBe(6);
   });
 
-  it('Turn off discrete zoom and zoom to a non-integer value', function (done) {
+  it('Turn off discrete zoom and zoom to a non-integer value', function () {
     map.discreteZoom(false);
     expect(map.discreteZoom()).toBe(false);
-    map.geoOff(geo.event.transitionend)
-      .geoOn(geo.event.transitionend, function () {
-        expect(map.zoom()).toBe(5.75);
-        done();
-      });
     map.zoom(5.75);
+    expect(map.zoom()).toBe(5.75);
   });
 
-  it('Turn back on discrete zoom', function (done) {
-    map.geoOff(geo.event.transitionend)
-      .geoOn(geo.event.transitionend, function () {
-        expect(map.discreteZoom()).toBe(true);
-        expect(map.zoom()).toBe(6);
-        done();
-      });
+  it('Turn back on discrete zoom', function () {
     map.discreteZoom(true);
+    expect(map.zoom()).toBe(6);
   });
 
-  it('Turn on parallel projection', function (done) {
+  it('Turn on parallel projection', function () {
     cam = map.baseLayer().renderer().contextRenderer().camera();
     var proj = cam.projectionMatrix();
     expect(proj[1] === 0 && proj[2] === 0 && proj[3] === 0 && proj[4] === 0 &&
            proj[6] === 0 && proj[7] === 0 && proj[8] === 0 && proj[9] === 0 &&
            proj[11] === 0 && proj[15] === 1).toBe(false);
+
     expect(map.parallelProjection()).toBe(false);
-    map.geoOff(geo.event.transitionend)
-      .geoOn(geo.event.transitionend, function () {
-        expect(map.parallelProjection()).toBe(true);
-        var proj = cam.projectionMatrix();
-        expect(proj[1] === 0 && proj[2] === 0 && proj[3] === 0 &&
-               proj[4] === 0 && proj[6] === 0 && proj[7] === 0 &&
-               proj[8] === 0 && proj[9] === 0 && proj[11] === 0 &&
-               proj[15] === 1).toBe(true);
-        done();
-      });
     map.parallelProjection(true).draw();
+
+    expect(map.parallelProjection()).toBe(true);
+    proj = cam.projectionMatrix();
+    expect(proj[1] === 0 && proj[2] === 0 && proj[3] === 0 &&
+           proj[4] === 0 && proj[6] === 0 && proj[7] === 0 &&
+           proj[8] === 0 && proj[9] === 0 && proj[11] === 0 &&
+           proj[15] === 1).toBe(true);
   });
 });
