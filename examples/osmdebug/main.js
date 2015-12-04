d06fb4e9664507f8d3e710f201cfbf57364b6532
David Manthey <david.manthey@kitware.com>
2015-12-04 16:24:50 -0500

    Add more options to the osmdebug example.

--- examples/osmdebug/main.js
+++ examples/osmdebug/main.js
@@ -3,13 +3,17 @@
  *      to draw borders on each tile when using the html renderer.  'all' to
  *      show both labels and borders.  These options just add a class to the
  *      #map element to invoke special css rules.
- *  renderer: 'vgl' (default), 'd3', 'null', or 'html'.  This picks the
- *      renderer for map tiles.  null or html uses the html renderer.
  *  lower: 'true' (default) or 'false'.  Keep all lower-level tiles if true.
  *      'false' was the old behavior where fewer tiles are rendered, and
  *      panning shows blank areas.
+ *  opacity: a css opacity value (typically a float from 0 to 1).
+ *  renderer: 'vgl' (default), 'd3', 'null', or 'html'.  This picks the
+ *      renderer for map tiles.  null or html uses the html renderer.
  *  url: url to use for the map files.  Placeholders are allowed.  Default is
  *      http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png
+ *  x: map center x
+ *  y: map center y
+ *  zoom: starting zoom level
  */
 
 var osmDebug = {};
@@ -18,33 +22,38 @@ var osmDebug = {};
 $(function () {
   'use strict';
 
+  // parse query parameters into an object for ease of access
+  var query = document.location.search.replace(/(^\?)/, '').split(
+    '&').map(function (n) {
+      n = n.split('=');
+      this[n[0]] = decodeURIComponent(n[1]);
+      return this;
+    }.bind({}))[0];
+
   // Create a map object
   var map = geo.map({
     node: '#map',
     center: {
-      x: -98.0,
-      y: 39.5
+      x: query.x !== undefined ? parseFloat(query.x) : -98.0,
+      y: query.y !== undefined ? parseFloat(query.y) : 39.5
     },
-    zoom: 3
+    zoom: query.zoom !== undefined ? parseFloat(query.zoom) : 3
   });
 
-  var debug = (location.search.match(/[?&]debug=([^&]+)(&|$)/) || [])[1];
-  $('#map').toggleClass('debug-label', (debug === 'true' || debug === 'all'))
-    .toggleClass('debug-border', (debug === 'border' || debug === 'all'));
+  $('#map').toggleClass('query.debug-label', (
+      query.debug === 'true' || query.debug === 'all'))
+    .toggleClass('query.debug-border', (
+      query.debug === 'border' || query.debug === 'all'));
   var params = {
-    renderer: (location.search.match(/[?&]renderer=([^&]+)(&|$)/
-      ) || [])[1] || 'vgl',
-    keepLower: (location.search.match(/[?&]lower=([^&]+)(&|$)/
-      ) || [])[1] === 'false' ? false : true,
-    opacity: (location.search.match(/[?&]opacity=([^&]+)(&|$)/
-      ) || [])[1] || '1'
+    renderer: query.renderer || 'vgl',
+    keepLower: query.lower === 'false' ? false : true,
+    opacity: query.opacity || '1'
   };
   if (params.renderer === 'null' || params.renderer === 'html') {
     params.renderer = null;
   }
-  var url = (location.search.match(/[?&]url=([^&]+)(&|$)/) || [])[1];
-  if (url) {
-    params.url = decodeURIComponent(url);
+  if (query.url) {
+    params.url = query.url;
   } else {
     params.baseUrl = 'http://otile1.mqcdn.com/tiles/1.0.0/map/';
   }
