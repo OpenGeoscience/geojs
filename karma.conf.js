e78e12206d495e536874cc96dc79b52c5149f485
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-02-02 12:30:50 -0500

    Fixed coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,17 @@
 // Defines a test server for running jasmine unit tests
 // with coverage support.
 
+var path = require('path');
+
 // share configuration with the main webpack builder
 var webpack_config = require('./webpack.config');
+
+/**
+ * Return URL friendly browser string
+ */
+function browser(b) {
+  return b.toLowerCase().split(/[ /-]/)[0];
+}
 
 module.exports = function (config) {
   config.set({
@@ -21,8 +30,9 @@ module.exports = function (config) {
     },
     coverageReporter: {
       reporters: [
-        {type: 'lcov', dir: 'dist/coverage/'},
-        {type: 'cobertura', dir: '.', file: 'coverage.txt'}
+        {type: 'html', dir: 'dist/coverage/', subdir: browser},
+        {type: 'cobertura', dir: 'dist/cobertura/', file: 'coverage.xml', subdir: browser},
+        {type: 'text-summary'}
       ]
     },
     frameworks: [
@@ -32,15 +42,11 @@ module.exports = function (config) {
       cache: true,
       devtool: 'inline-source-map',
       module: {
-        preloaders: [
+        preLoaders: [
           {
             test: /\.js$/,
-            include: /src/,
-            exclude: /(node_modules|bower_components|testing)/,
-            loader: 'istanbul',
-            query: {
-              cacheDirectory: true
-            }
+            include: path.resolve('src/'),
+            loader: 'istanbul-instrumenter'
           }
         ],
         loaders: webpack_config.module.loaders
