087a062a3a054f99fa481e48320676bd128b4c73
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-02-03 09:25:12 -0500

    Create a separate config for coverage testing

--- karma-cov.conf.js
+++ karma-cov.conf.js
@@ -1,8 +1,17 @@
 // Defines a test server for running jasmine unit tests
 // with coverage support.
 
+var path = require('path');
+
 // share configuration with the main webpack builder
 var webpack_config = require('./webpack.config');
+
+/**
+ * Return URL friendly browser string
+ */
+function browser(b) {
+  return b.toLowerCase().split(/[ /-]/)[0];
+}
 
 module.exports = function (config) {
   config.set({
@@ -17,7 +26,14 @@ module.exports = function (config) {
       'kjhtml'
     ],
     preprocessors: {
-      'tests/cases/**/*.js': ['webpack', 'sourcemap']
+      'tests/**/*.js': ['webpack', 'sourcemap']
+    },
+    coverageReporter: {
+      reporters: [
+        {type: 'html', dir: 'dist/coverage/', subdir: browser},
+        {type: 'cobertura', dir: 'dist/cobertura/', file: 'coverage.xml', subdir: browser},
+        {type: 'text-summary'}
+      ]
     },
     frameworks: [
       'jasmine'
@@ -26,6 +42,13 @@ module.exports = function (config) {
       cache: true,
       devtool: 'inline-source-map',
       module: {
+        preLoaders: [
+          {
+            test: /\.js$/,
+            include: path.resolve('src/'),
+            loader: 'istanbul-instrumenter'
+          }
+        ],
         loaders: webpack_config.module.loaders
       },
       resolve: webpack_config.resolve
