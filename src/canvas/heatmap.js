c9084304aef6edca42dd87f423f245d92709e942
Derek Johnston <derek@epidemico.com>
2016-02-29 15:49:21 -0500

    Imported heatmap example, source

---
+++ src/canvas/heatmap.js
@@ -0,0 +1,173 @@
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * Create a new instance of class heatmap
+ *
+ * @class
+ * @param {Object} arg Options object
+ * @extends geo.heatmap
+ * @returns {geo.canvas.heatmap}
+ */
+//////////////////////////////////////////////////////////////////////////////
+geo.canvas.heatmap = function (arg) {
+  'use strict';
+
+  if (!(this instanceof geo.canvas.heatmap)) {
+    return new geo.canvas.heatmap(arg);
+  }
+  geo.heatmap.call(this, arg);
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  var m_this = this,
+      s_exit = this._exit,
+      s_init = this._init,
+      s_update = this._update;
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Build
+   * @override
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._build = function () {
+    s_update.call(m_this);
+    return m_this;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Meta functions for converting from geojs styles to canvas.
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._convertColor = function (c) {
+    var rgb;
+    if (c.hasOwnProperty('r') &&
+        c.hasOwnProperty('g') &&
+        c.hasOwnProperty('b')) {
+      rgb = d3.rgb(255 * c.r, 255 * c.g, 255 * c.b).toString();
+    }
+    return rgb;
+  };
+
+  this._gradient = function () {
+    var canvas = document.createElement('canvas'),
+      context = canvas.getContext('2d'),
+      gradient = context.createLinearGradient(0, 0, 0, 256),
+      colors = m_this.style('color');
+
+      canvas.width = 1;
+      canvas.height = 256;
+
+      for (var stop in colors) {
+        gradient.addColorStop(stop, m_this._convertColor(colors[stop]));
+      }
+
+      context.fillStyle = gradient;
+      context.fillRect(0, 0, 1, 256);
+
+      m_this._grad = context.getImageData(0, 0, 1, 256).data;
+
+      return m_this;
+  };
+
+  this._radius = function () {
+    var circle = m_this._circle = document.createElement('canvas'),
+      ctx = circle.getContext('2d'),
+      r = m_this.style('radius'),
+      blur = m_this.style('blurRadius');
+
+    var r2 = blur + r;
+
+    circle.width = circle.height = r2 * 2;
+    ctx.shadowOffsetX = ctx.shadowOffsetY = r2 * 2;
+    ctx.shadowBlur = blur;
+    ctx.shadowColor = 'black';
+
+    ctx.beginPath();
+    ctx.arc(-r2, -r2, r, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+
+    return m_this;
+  };
+
+  this._colorize = function (pixels, gradient) {
+    for (var i = 0; i < pixels.length; i+=4) {
+      var j = pixels[i + 3] * 4;
+      if (j) {
+        console.log(pixels[i+3], gradient, gradient[i+3]);
+      }
+      if (j) {
+        pixels[i] = gradient[j];
+        pixels[i+1] = gradient[j+1];
+        pixels[i+2] = gradient[j+2];
+      }
+    }
+  };
+
+  this._renderOnCanvas = function (context2d, map) {
+    var data = m_this.data() || [];
+    m_this._radius();
+    m_this._gradient();
+    var radius = m_this.style('radius');
+    data.map(m_this.position()).map(function (p) {
+      var p = m_this.layer().map().gcsToDisplay(p, null);
+      context2d.globalAlpha = Math.max(.05)
+      context2d.drawImage(m_this._circle, p.x - radius, p.y - radius);
+    });
+
+    var pixelArray = context2d.getImageData(0, 0, 1000, 1000);
+    m_this._colorize(pixelArray.data, m_this._grad);
+    context2d.putImageData(pixelArray, 0, 0)
+    return m_this;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Initialize
+   * @protected
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._init = function () {
+    s_init.call(m_this, arg); //doesn't this get called implicitly by geo.heatmap.call TODO ?
+    return m_this;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Update
+   * @protected
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._update = function () {
+    s_update.call(m_this);
+    if (m_this.buildTime().getMTime() <= m_this.dataTime().getMTime() ||
+        m_this.updateTime().getMTime() < m_this.getMTime()) {
+      m_this._build();
+    }
+    m_this.updateTime().modified();
+    return m_this;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Destroy
+   * @protected
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._exit = function () {
+    s_exit.call(m_this);
+  };
+
+  m_this._init(arg);
+  return this;
+};
+
+inherit(geo.canvas.heatmap, geo.heatmap);
+
+// Now register it
+geo.registerFeature('canvas', 'heatmap', geo.canvas.heatmap);
---
+++ src/core/heatmap.js
@@ -0,0 +1,102 @@
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * Create a new instance of class heatmap
+ *
+ * @class
+ * @extends geo.feature
+ * @returns {geo.heatmap}
+ *
+ */
+//////////////////////////////////////////////////////////////////////////////
+geo.heatmap = function (arg) {
+  'use strict';
+  if (!(this instanceof geo.heatmap)){
+    return new geo.heatmap(arg);
+  }
+  arg = arg || {};
+  geo.feature.call(this, arg);
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * @private
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  var m_this = this,
+      m_position,
+      m_intensity,
+      s_init = this._init,
+      s_data = this.data;
+
+  m_position = arg.position || function (d) { return d; };
+  m_intensity = arg.intensity || function (d) { return 1; };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Get/Set position accessor
+   *
+   * @returns {geo.heatmap}
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this.position = function (val) {
+    if (val === undefined) {
+      return m_position;
+    } else {
+      m_position = val;
+      m_this.dataTime().modified();
+      m_this.modified();
+    }
+    return m_this;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Get/Set intensity
+   *
+   * @returns {geo.heatmap}
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this.intensity = function (val) {
+    if (val === undefined) {
+      return m_intensity;
+    } else {
+      m_intensity = val;
+      m_this.dataTime().modified();
+      m_this.modified();
+    }
+    return m_this;
+  };
+
+  ////////////////////////////////////////////////////////////////////////////
+  /**
+   * Initialize
+   */
+  ////////////////////////////////////////////////////////////////////////////
+  this._init = function (arg) {
+    s_init.call(m_this, arg);
+
+    var defaultStyle = $.extend(
+      {},
+      {
+        opacity: 1,
+        radius: 10,
+        blurRadius: 10,
+        blur: 'Gaussian',
+        color: {.25: {r: 0, g: 0, b: 1}, .5: {r: 0, g: 1, b: 1}, .75: {r: 1, g: 1, b: 0}, 1: {r: 1, g: 0, b: 0}},
+        maxIntensity: 1
+      },
+      arg.style === undefined ? {} : arg.style
+    );
+
+    m_this.style(defaultStyle);
+
+    if (m_position) {
+      m_this.dataTime().modified();
+    }
+  };
+
+  this._init(arg);
+  return this;
+
+};
+
+inherit(geo.heatmap, geo.feature)
