35501c37f6ad4b994535eaeb8917c97e9508dd01
David Manthey <david.manthey@kitware.com>
2016-03-03 13:48:44 -0500

    Always create a notes file and submit it.
    Apparently I can't test if a file exists in CTestConfig in a useful manner.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -14,6 +14,4 @@ set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=geojs")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
-if(EXISTS "${CTEST_BINARY_DIRECTORY}/build_notes.json")
-    set(CTEST_NOTES_FILES "${CTEST_BINARY_DIRECTORY}/build_notes.json")
-endif()
+set(CTEST_NOTES_FILES "${CTEST_BINARY_DIRECTORY}/build_notes.json")
