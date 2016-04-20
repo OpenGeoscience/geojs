4b1ba980ecdd579a62d560c3cc0fef10d21d280f
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-04-20 14:47:05 -0400

    Add git sha to the built bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
+var exec = require('child_process').execSync;
+var sha = '';
+
+try {
+  sha = exec('git rev-parse HEAD', {cwd: __dirname}).toString().trim();
+} catch (e) {
+  console.warn('Could not determine git hash.');
+}
+
 var define_plugin = new webpack.DefinePlugin({
-  VERSION: JSON.stringify(require('./package.json').version)
+  GEO_SHA: JSON.stringify(sha),
+  GEO_VERSION: JSON.stringify(require('./package.json').version)
 });
 
 module.exports = {
