6b30619e95c44cb383e310f7a0da3b053c8372ed
Derek Johnston <derek@epidemico.com>
2016-09-13 11:10:43 -0400

    add mock data for example

--- examples/grid/main.js
+++ examples/grid/main.js
@@ -17,22 +17,27 @@ $(function () {
     opacity: 0.75
   };
   var gridOptions = {
-    minIntensity: null,
-    maxIntensity: null,
+    minIntensity: 0,
+    maxIntensity: 1,
     style: {
       color: {
         0.00: {r: 0, g: 0, b: 0, a: 0.0},
         0.25: {r: 0, g: 1, b: 0, a: 0.5},
         0.50: {r: 1, g: 1, b: 0, a: 0.8},
         1.00: {r: 1, g: 0, b: 0, a: 1.0}
-      },
+      }
     },
+    upperLeft: [0, 0],
+    rowCount: 1E5,
     updateDelay: 50
   };
   map.createLayer('osm');
   layer = map.createLayer('feature', layerOptions);
   grid = layer.createFeature('grid', gridOptions)
-
+  grid.data(Array(1E6).fill(0).map(function () {
+    return Math.random()
+  }))
+  grid.draw()
   /* Make some values available in the global context so curious people can
    * play with them. */
   window.grid = {
