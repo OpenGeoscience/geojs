d95f8aef483615a302aad3ce97f8e4975a811e62
David Manthey <david.manthey@kitware.com>
2017-02-28 08:59:58 -0500

    Add a screenshot button in the examples header.

--- examples/common/js/examples.js
+++ examples/common/js/examples.js
@@ -34,3 +34,23 @@ var exampleUtils = {
 };
 
 window.utils = exampleUtils;
+
+/* Add a function to take a screenshot.  Show the screenshot so that a user can
+ * click on it to save it or right-click to copy it. */
+$(function () {
+  $('.gj-screenshot-link').on('click', function () {
+    $('.gj-screenshot-result').hide();
+    $('.gj-screenshot-waiting').show();
+    var map = $('#map').data('data-geojs-map');
+    map.screenshot({wait:'idle'}).then(function (res) {
+      $('.gj-screenshot-waiting').hide();
+      $('.gj-screenshot-result img').attr({src: res});
+      $('.gj-screenshot-download').attr({href: res});
+      $('.gj-screenshot-result').show();
+      /* If you want to trigger an automatic download, enable this line (and,
+       * perhaps, always hide the waiting and result elements).
+      $('.gj-screenshot-download')[0].click();
+       */
+    });
+  });
+});
