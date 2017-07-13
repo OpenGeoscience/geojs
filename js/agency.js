7c83f661836915bcf7df83008a62119d728e00cb
Matthew Ma <matthew.ma@kitware.com>
2017-07-13 14:47:37 -0400

    starts to utilize Jekyll

--- js/agency.js
+++ js/agency.js
@@ -4,13 +4,13 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    // $('a.page-scroll').bind('click', function(event) {
+    //     var $anchor = $(this);
+    //     $('html, body').stop().animate({
+    //         scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    //     }, 1250, 'easeInOutExpo');
+    //     event.preventDefault();
+    // });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
