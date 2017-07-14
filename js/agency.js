cadd0ea4f706bb4900f8139dac0178ec68de0a5e
Matthew Ma <matthew.ma@kitware.com>
2017-07-14 15:09:41 -0400

    initial implementation of tutorials page

--- js/agency.js
+++ js/agency.js
@@ -3,21 +3,6 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    // $('a.page-scroll').bind('click', function(event) {
-    //     var $anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    //     }, 1250, 'easeInOutExpo');
-    //     event.preventDefault();
-    // });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){ 
             $('.navbar-toggle:visible').click();
