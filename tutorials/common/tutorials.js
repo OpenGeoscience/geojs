5e46023fb9dfef9ff608cf9c0ff6d4c403249cce
David Manthey <david.manthey@kitware.com>
2017-08-04 16:16:57 -0400

    Fix import order in tutorials.
    geo.ext.js has to be imported before geo.js for Hammer to be used.  With this change, the tutorials work on an iPad.

--- tutorials/common/tutorials.js
+++ tutorials/common/tutorials.js
@@ -13,8 +13,8 @@ var processBlockInfo = {
     /* We could also load from the CDN:
      *   https://cdnjs.cloudflare.com/ajax/libs/geojs/0.12.2/geo.min.js
      * or the non-minified versions to make debug easier. */
-    '  <script type="text/javascript" src="../../built/geo.min.js"></script>\n' +
     '  <script type="text/javascript" src="../../built/geo.ext.min.js"></script>\n' +
+    '  <script type="text/javascript" src="../../built/geo.min.js"></script>\n' +
     '</head>\n' +
     '<body>\n' +
     '  <div id="map"></div>\n' +
