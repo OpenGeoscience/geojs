fc1d23662bfe2d70e2058a5da03a65e8e8b5467f
David Manthey <david.manthey@kitware.com>
2017-10-13 13:17:06 -0400

    Automatically generate example and tutorial lists.
    Reduce code duplication in the example and tutorial build scripts.  This moves the enumeration, standardization, and sorting of the tutorial and examples to a single location (examples/build-utils.js).

--- examples/build-website.js
+++ examples/build-website.js
@@ -1,74 +1,49 @@
 var path = require('path');
-var glob = require('glob').sync;
 var fs = require('fs-extra');
 var docco = require('docco').document;
 var pug = require('pug');
 
+var buildUtils = require('./build-utils');
+
 // generate the examples
 fs.ensureDirSync('website/source/examples');
-glob('examples/*/example.json')
-  .map(function (f) {
-    // /path/to/example.json
-    f = path.resolve(f);
+var examples = buildUtils.getList('examples', 'example', path.resolve('website', 'source'));
+examples.map(function (json) {
+  // make docco documentation in:
+  //   dist/examples/<name>/docs/
+  if (json.main) {
+    docco({
+      args: [json.main],
+      output: path.resolve(json.output, 'docs'),
+      layout: 'classic'
+    }, function () {
+      // simplify the docco output to reduce the output size by
+      // removing the unnecessary public/ directory
+      fs.removeSync(path.resolve(json.output, 'docs', 'public'));
+    });
+  }
+  json.docHTML = 'docs/' + path.basename(json.main).replace(/js$/, 'html');
 
-    // content of example.json
-    var json = fs.readJSONSync(path.resolve(f));
+  var pugTemplate = '';
+  var pugFile = path.relative('.', path.resolve(json.dir, 'index.pug'));
+  if (fs.existsSync(path.resolve(json.dir, 'index.pug'))) {
+    pugTemplate = fs.readFileSync(pugFile, 'utf8');
+    pugTemplate = pugTemplate.replace('extends ../common/index.pug', '');
+    pugTemplate = pugTemplate.replace('block append mainContent', '');
+  }
+  pugTemplate = 'div' + pugTemplate;
 
-    // directory of the example
-    var dir = path.dirname(f);
-
-    // by default, assume the path is where the files are located
-    json.path = json.path || path.basename(dir);
-    json.exampleCss = json.exampleCss || [];
-    json.exampleJs = json.exampleJs || [];
-
-    // the main js file for the example
-    var main = path.resolve(dir, json.exampleJs[0]);
-
-    // the output directory where the example will be compiled
-    var output = path.resolve('website', 'source', 'examples', json.path);
-
-    // create, empty, and copy the source directory
-    fs.emptyDirSync(output);
-    fs.copySync(dir, output);
-
-    // make docco documentation in:
-    //   dist/examples/<name>/docs/
-    if (json.exampleJs.length) {
-      docco({
-        args: [main],
-        output: path.resolve(output, 'docs'),
-        layout: 'classic'
-      }, function () {
-        // simplify the docco output to reduce the output size by
-        // removing the unnecessary public/ directory
-        fs.removeSync(path.resolve(output, 'docs', 'public'));
-      });
-    }
-    json.docHTML = 'docs/' + path.basename(main).replace(/js$/, 'html');
-
-    json.bundle = '../bundle.js';
-
-    var pugTemplate = '';
-    var pugFile = path.relative('.', path.resolve(dir, 'index.pug'));
-    if (fs.existsSync(path.resolve(dir, 'index.pug'))) {
-      pugTemplate = fs.readFileSync(pugFile, 'utf8');
-      pugTemplate = pugTemplate.replace('extends ../common/index.pug', '');
-      pugTemplate = pugTemplate.replace('block append mainContent', '');
-    }
-    pugTemplate = 'div' + pugTemplate;
-
-    var fn = pug.compile(pugTemplate, { pretty: false });
-    var html = fn(json);
-    html = `---
-layout: example 
+  var fn = pug.compile(pugTemplate, { pretty: false });
+  var html = fn(json);
+  html = `---
+layout: example
 title: ${json.title}
 about: ${json.about.text}
 exampleCss: ${JSON.stringify(json.exampleCss)}
 exampleJs: ${JSON.stringify(json.exampleJs)}
 ---
 ` + html;
-    fs.writeFileSync(path.resolve(output, 'index.html'), html);
-    return json;
-  });
+  fs.writeFileSync(path.resolve(json.output, 'index.html'), html);
+});
 
+buildUtils.writeYamlList(path.resolve('website', 'source', '_data'), 'examples.yml', examples);
--- tutorials/build.js
+++ tutorials/build.js
@@ -1,54 +1,16 @@
 var path = require('path');
-var glob = require('glob').sync;
 var fs = require('fs-extra');
 var pug = require('pug');
 
+var buildUtils = require('../examples/build-utils');
+
 // generate the tutorials
 fs.ensureDirSync('dist/tutorials');
-var tutorials = glob('tutorials/*/tutorial.json')
-  .map(function (f) {
-    // /path/to/tutorial.json
-    f = path.resolve(f);
+var tutorials = buildUtils.getList('tutorials', 'tutorial', 'dist');
 
-    // content of tutorial.json
-    var json = fs.readJSONSync(path.resolve(f));
-
-    // directory of the tutorial
-    var dir = path.dirname(f);
-
-    // by default, assume the path is where the files are located.
-    json.path = json.path || path.basename(dir);
-    json.tutorialCss = json.tutorialCss || [];
-    json.tutorialJs = json.tutorialJs || [];
-    json.bundle = '../bundle.js';
-
-    // the output directory where the tutorial will be compiled
-    var output = path.resolve('dist', 'tutorials', json.path);
-
-    // create, empty, and copy the source directory
-    fs.emptyDirSync(output);
-    fs.copySync(dir, output);
-
-    var fn = pug.compileFile(path.relative('.', path.resolve(dir, 'index.pug')), {pretty: true});
-    fs.writeFileSync(path.resolve(output, 'index.html'), fn(json));
-    return json;
-  });
-
-/* Sort tutorials.  Tutorials are sorted by level, order, title, and path.
- * undefined or null levels are orders are sorted after defined values.  level
- * should be used for the approximate difficulty of the tutorial, and order for
- * making specific tutorials appear sooner in the list. */
-tutorials.sort(function (a, b) {
-  if (a.level !== b.level) {
-    return a.level === undefined ? 1 : b.level === undefined ? -1 : a.level - b.level;
-  }
-  if (a.order !== b.order) {
-    return a.order === undefined ? 1 : b.order === undefined ? -1 : a.order - b.order;
-  }
-  if (a.title !== b.title) {
-    return a.title < b.title ? -1 : 1;
-  }
-  return a.path < b.path ? -1 : 1;
+tutorials.map(function (json) {
+  var fn = pug.compileFile(path.relative('.', path.resolve(json.dir, 'index.pug')), {pretty: true});
+  fs.writeFileSync(path.resolve(json.output, 'index.html'), fn(json));
 });
 
 // copy common files
