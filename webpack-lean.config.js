6f8e7e93183e8745745dabb389d42d0e9d0edefb
Zach Mullen <zach.mullen@kitware.com>
2018-08-07 11:55:35 -0400

    Build two bundles: full and lean
    The lean bundle does not include any of the runtime packages
    (currently just hammerjs and d3) and assumes they'll be available
    at runtime in the global namespace.

---
+++ webpack-lean.config.js
@@ -0,0 +1,23 @@
+// The lean build does not include any of the third-party runtime dependencies, rather
+// it assume they are in the global namespace at runtime.
+var config = require('./webpack.base.config');
+var merge = require('webpack-merge');
+
+module.exports = merge(config, {
+  entry: {
+    'geo.lean': './index.js',
+    'geo.lean.min': './index.js'
+  },
+  externals: {
+    d3: 'd3',
+    hammerjs: {
+      root: 'Hammer',
+      commonjs: 'hammerjs',
+      commonjs2: 'hammerjs',
+      amd: 'hammerjs',
+      // Since GeoJS's libraryTarget is "umd", defining this (undocumented) external library type
+      // will allow Webpack to create a better error message if a "hammerjs" import fails
+      umd: 'hammerjs'
+    }
+  }
+});
